8b5bbb53ef1d1d0e7f359c5b0798ce074a57e138
AliLogic <drfiniz@gmail.com>
2019-12-18 11:20:50 +0500

    added /gotoxyz, /slap, /goto, /get and /warp

--- packages/orp/server/admin/cmds_s.lua
+++ packages/orp/server/admin/cmds_s.lua
@@ -1,18 +1,111 @@
 local colour = ImportPackage("colours")
 
+AddCommand("warp", function (playerid, fromid, toid)
+
+	if (PlayerData[playerid].admin < 1) then
+		return AddPlayerChat(playerid, "<span color=\""..colour.COLOUR_LIGHTRED().."\">Error: You don't have permission to use this command.</>")
+	end
+
+	if (fromid == nil or toid == nil) then
+		return AddPlayerChat(playerid, "<span color=\""..colour.COLOUR_LIGHTRED().."\">Usage:</> /warp <playerid> <playerid>")
+	end
+
+	if (not IsValidPlayer(fromid)) or (not IsValidPlayer(toid)) then
+		return AddPlayerChat(playerid, "<span color=\""..colour.COLOUR_LIGHTRED().."\">Error: Invalid player ID entered.</>")
+	end
+
+	local x, y, z = GetPlayerLocation(toid)
+	SetPlayerLocation(fromid, x, y, z + 10.0)
+
+	return AddPlayerChat(playerid, "Teleported to: "..GetPlayerName(fromid).." ("..fromid..") to "..GetPlayerName(fromid).." ("..fromid..").")
+end)
+
+AddCommand("gotoxyz", function (playerid, x, y, z)
+
+	if (PlayerData[playerid].admin < 1) then
+		return AddPlayerChat(playerid, "<span color=\""..colour.COLOUR_LIGHTRED().."\">Error: You don't have permission to use this command.</>")
+	end
+
+	if (x == nil or y == nil or z == nil) then
+		return AddPlayerChat(playerid, "<span color=\""..colour.COLOUR_LIGHTRED().."\">Usage:</> /goto <x> <y> <z>")
+	end
+
+	SetPlayerLocation(playerid, x, y, z)
+
+	return AddPlayerChat(playerid, "Teleported to: "..x..", "..y..", "..z..".")
+end)
+
+AddCommand("slap", function (playerid, lookupid)
+
+	if (PlayerData[playerid].admin < 1) then
+		return AddPlayerChat(playerid, "<span color=\""..colour.COLOUR_LIGHTRED().."\">Error: You don't have permission to use this command.</>")
+	end
+
+	if (lookupid == nil) then
+		return AddPlayerChat(playerid, "<span color=\""..colour.COLOUR_LIGHTRED().."\">Usage:</> /slap <playerid>")
+	end
+
+	if not IsValidPlayer(lookupid) then
+		return AddPlayerChat(playerid, "<span color=\""..colour.COLOUR_LIGHTRED().."\">Error: Invalid player ID entered.</>")
+	end
+
+	Slap(lookupid)
+	return AddPlayerChat(playerid, "You slapped "..GetPlayerName(lookupid).." ("..lookupid..").")
+end)
+
+AddCommand("goto", function (playerid, lookupid)
+
+	if (PlayerData[playerid].admin < 1) then
+		return AddPlayerChat(playerid, "<span color=\""..colour.COLOUR_LIGHTRED().."\">Error: You don't have permission to use this command.</>")
+	end
+
+	if (lookupid == nil) then
+		return AddPlayerChat(playerid, "<span color=\""..colour.COLOUR_LIGHTRED().."\">Usage:</> /goto <playerid>")
+	end
+
+	if not IsValidPlayer(lookupid) then
+		return AddPlayerChat(playerid, "<span color=\""..colour.COLOUR_LIGHTRED().."\">Error: Invalid player ID entered.</>")
+	end
+
+	local x, y, z = GetPlayerLocation(lookupid)
+	SetPlayerLocation(playerid, x, y, z + 10.0)
+
+	return AddPlayerChat(playerid, "Teleported to: "..GetPlayerName(lookupid).." ("..lookupid..").")
+end)
+
+AddCommand("get", function (playerid, lookupid)
+
+	if (PlayerData[playerid].admin < 1) then
+		return AddPlayerChat(playerid, "<span color=\""..colour.COLOUR_LIGHTRED().."\">Error: You don't have permission to use this command.</>")
+	end
+
+	if (lookupid == nil) then
+		return AddPlayerChat(playerid, "<span color=\""..colour.COLOUR_LIGHTRED().."\">Usage:</> /get <playerid>")
+	end
+
+	if not IsValidPlayer(lookupid) then
+		return AddPlayerChat(playerid, "<span color=\""..colour.COLOUR_LIGHTRED().."\">Error: Invalid player ID entered.</>")
+	end
+
+	local x, y, z = GetPlayerLocation(playerid)
+	SetPlayerLocation(lookupid, x, y, z + 10.0)
+
+	return AddPlayerChat(playerid, "You Teleported "..GetPlayerName(lookupid).." ("..lookupid..") to yourself.")
+end)
+
 AddCommand("apos", function (player, id)
-    if (PlayerData[player].admin < 5) then
-        return AddPlayerChat(player, "<span color=\""..colour.COLOUR_LIGHTRED().."\">Error: You don't have permission to use this command.</>")
-    end
+	if (PlayerData[player].admin < 5) then
+		return AddPlayerChat(player, "<span color=\""..colour.COLOUR_LIGHTRED().."\">Error: You don't have permission to use this command.</>")
+	end
 
 	local x, y, z = GetPlayerLocation(player)
 	return AddPlayerChat(player, "X: "..x.." Y: "..y.." Z: "..z)
 end)
 
 AddCommand("av", function (player, model)
-    if (PlayerData[player].admin < 2) then
-        return AddPlayerChat(player, "<span color=\""..colour.COLOUR_LIGHTRED().."\">Error: You don't have permission to use this command.</>")
-    end
+	if (PlayerData[player].admin < 2) then
+		return AddPlayerChat(player, "<span color=\""..colour.COLOUR_LIGHTRED().."\">Error: You don't have permission to use this command.</>")
+	end
 
 	if (model == nil) then
 		return AddPlayerChat(player, "<span color=\""..colour.COLOUR_LIGHTRED().."\">Usage:</> /av <model>")
@@ -41,169 +134,169 @@ AddCommand("av", function (player, model)
 		SetVehicleLicensePlate(vehicle, "EMS-02")
 	end
 
-    -- Set us in the driver seat
+	-- Set us in the driver seat
 	SetPlayerInVehicle(player, vehicle)
 	AddPlayerChat(player, "Vehicle spawned! (New ID: "..vehicle..")")
 end)
 
 AddCommand("asetadmin", function (player, target, level)
-    if (PlayerData[player].admin < 5) then
-        return AddPlayerChat(player, "<span color=\""..colour.COLOUR_LIGHTRED().."\">Error: You don't have permission to use this command.</>")
-    end
+	if (PlayerData[player].admin < 5) then
+		return AddPlayerChat(player, "<span color=\""..colour.COLOUR_LIGHTRED().."\">Error: You don't have permission to use this command.</>")
+	end
 
-    if target == nil or level == nil then
-        return AddPlayerChat(player, "<span color=\""..colour.COLOUR_LIGHTRED().."\">Usage:</> /asetadmin <playerid> <level>")
-    end
+	if target == nil or level == nil then
+		return AddPlayerChat(player, "<span color=\""..colour.COLOUR_LIGHTRED().."\">Usage:</> /asetadmin <playerid> <level>")
+	end
 
-    if IsValidPlayer(target) == nil then
-        return AddPlayerChat(player, "<span color=\""..colour.COLOUR_LIGHTRED().."\">Error: Invalid player ID entered.</>")
-    end
-    
-    target = tonumber(target)
+	if IsValidPlayer(target) == nil then
+		return AddPlayerChat(player, "<span color=\""..colour.COLOUR_LIGHTRED().."\">Error: Invalid player ID entered.</>")
+	end
 
-    if PlayerData[target].logged_in == false then
-        return AddPlayerChat(player, "<span color=\""..colour.COLOUR_LIGHTRED().."\">Error: This player is not logged in.</>")
-    end
+	target = tonumber(target)
 
-    level = tonumber(level)
+	if PlayerData[target].logged_in == false then
+		return AddPlayerChat(player, "<span color=\""..colour.COLOUR_LIGHTRED().."\">Error: This player is not logged in.</>")
+	end
 
-    if level > PlayerData[player].admin then
-        return AddPlayerChat(player, "<span color=\""..colour.COLOUR_LIGHTRED().."\">Error: You cannot set a level above yours.</>")
-    end
+	level = tonumber(level)
 
-    if level < 0 or level > 5 then
-        return AddPlayerChat(player, "<span color=\""..colour.COLOUR_LIGHTRED().."\">Error: Admin levels range from 0 - 5.</>")
-    end
+	if level > PlayerData[player].admin then
+		return AddPlayerChat(player, "<span color=\""..colour.COLOUR_LIGHTRED().."\">Error: You cannot set a level above yours.</>")
+	end
 
-    PlayerData[player].admin = level
+	if level < 0 or level > 5 then
+		return AddPlayerChat(player, "<span color=\""..colour.COLOUR_LIGHTRED().."\">Error: Admin levels range from 0 - 5.</>")
+	end
 
-    AddPlayerChat(player, string.format("<span color=\"%s\">You have set %s (%s, %d)'s admin rank to %s (%d).</>",
-        colour.COLOUR_YELLOW(), GetPlayerName(target), PlayerData[player].name, target, GetPlayerAdminRank(level), level))
+	PlayerData[player].admin = level
 
-    AddPlayerChat(player, string.format("<span color=\"%s\">%s %s has set your admin rank to %s (%d).</>",
-        colour.COLOUR_YELLOW(), GetPlayerAdminRank(player), PlayerData[player].name, GetPlayerAdminRank(level), level))
+	AddPlayerChat(player, string.format("<span color=\"%s\">You have set %s (%s, %d)'s admin rank to %s (%d).</>",
+		colour.COLOUR_YELLOW(), GetPlayerName(target), PlayerData[player].name, target, GetPlayerAdminRank(level), level))
+
+	AddPlayerChat(player, string.format("<span color=\"%s\">%s %s has set your admin rank to %s (%d).</>",
+		colour.COLOUR_YELLOW(), GetPlayerAdminRank(player), PlayerData[player].name, GetPlayerAdminRank(level), level))
 end)
 
 AddCommand("a", function (player, ...)
-    if (PlayerData[player].admin < 1) then
-        return AddPlayerChat(player, "<span color=\""..colour.COLOUR_LIGHTRED().."\">Error: You don't have permission to use this command.</>")
-    end
+	if (PlayerData[player].admin < 1) then
+		return AddPlayerChat(player, "<span color=\""..colour.COLOUR_LIGHTRED().."\">Error: You don't have permission to use this command.</>")
+	end
 
-    local args = {...}
+	local args = {...}
 
-    if args[1] == nil then
-        return AddPlayerChat(player, "<span color=\""..colour.COLOUR_LIGHTRED().."\">Usage:</> /a [text]")
-    end
+	if args[1] == nil then
+		return AddPlayerChat(player, "<span color=\""..colour.COLOUR_LIGHTRED().."\">Usage:</> /a [text]")
+	end
 
-    local text = ''
+	local text = ''
 
-    for _, v in pairs(args) do
-        text = text.." "..v
-    end
+	for _, v in pairs(args) do
+		text = text.." "..v
+	end
 
-    for _, i in pairs(GetAllPlayers()) do
-        if PlayerData[i].admin > 0 then
-            AddPlayerChat(player, string.format("<span color=\"%s\">** %s %s (%s, %d):%s</>",
-                colour.COLOUR_LIGHTRED(), GetPlayerAdminRank(player), GetPlayerName(player), PlayerData[player].name, player, text))
-        end
+	for _, i in pairs(GetAllPlayers()) do
+		if PlayerData[i].admin > 0 then
+			AddPlayerChat(player, string.format("<span color=\"%s\">** %s %s (%s, %d):%s</>",
+				colour.COLOUR_LIGHTRED(), GetPlayerAdminRank(player), GetPlayerName(player), PlayerData[player].name, player, text))
+		end
 	end
 
 end)
 
 AddCommand("astats", function (player, target)
-    if (PlayerData[player].admin < 2) then
-        return AddPlayerChat(player, "<span color=\""..colour.COLOUR_LIGHTRED().."\">Error: You don't have permission to use this command.</>")
-    end
+	if (PlayerData[player].admin < 2) then
+		return AddPlayerChat(player, "<span color=\""..colour.COLOUR_LIGHTRED().."\">Error: You don't have permission to use this command.</>")
+	end
 
-    if target == nil then
-        return AddPlayerChat(player, "<span color=\""..colour.COLOUR_LIGHTRED().."\">Usage:</> /astats <playerid>")
-    end
+	if target == nil then
+		return AddPlayerChat(player, "<span color=\""..colour.COLOUR_LIGHTRED().."\">Usage:</> /astats <playerid>")
+	end
 
-    if IsValidPlayer(target) == nil then
-        return AddPlayerChat(player, "<span color=\""..colour.COLOUR_LIGHTRED().."\">Error: Invalid player ID entered.</>")
-    end
+	if IsValidPlayer(target) == nil then
+		return AddPlayerChat(player, "<span color=\""..colour.COLOUR_LIGHTRED().."\">Error: Invalid player ID entered.</>")
+	end
 
-    target = tonumber(target)
+	target = tonumber(target)
 
-    if PlayerData[tonumber(target)].logged_in == false then
-        return AddPlayerChat(player, "<span color=\""..colour.COLOUR_LIGHTRED().."\">Error: This player is not logged in.</>")
-    end
+	if PlayerData[tonumber(target)].logged_in == false then
+		return AddPlayerChat(player, "<span color=\""..colour.COLOUR_LIGHTRED().."\">Error: This player is not logged in.</>")
+	end
 
-    ViewPlayerStats(player, target)
+	ViewPlayerStats(player, target)
 end)
 
 function cmd_acv(player, model, plate)
-    if (PlayerData[player].admin < 4) then
-        return AddPlayerChat(player, "<span color=\""..colour.COLOUR_LIGHTRED().."\">Error: You don't have permission to use this command.</>")
-    end
+	if (PlayerData[player].admin < 4) then
+		return AddPlayerChat(player, "<span color=\""..colour.COLOUR_LIGHTRED().."\">Error: You don't have permission to use this command.</>")
+	end
 
-    if model == nil or plate == nil then
-        return AddPlayerChat(player, "<span color=\""..colour.COLOUR_LIGHTRED().."\">Usage:</> /(ac)reate(v)ehicle <model> <plate>")
-    end
+	if model == nil or plate == nil then
+		return AddPlayerChat(player, "<span color=\""..colour.COLOUR_LIGHTRED().."\">Usage:</> /(ac)reate(v)ehicle <model> <plate>")
+	end
 
-    model = tonumber(model)
+	model = tonumber(model)
 
-    if model < 0 or model > 25 then
-        return AddPlayerChat(player, "<span color=\""..colour.COLOUR_LIGHTRED().."\">Error: Vehicle model "..model.." does not exist.</>")
-    end
+	if model < 0 or model > 25 then
+		return AddPlayerChat(player, "<span color=\""..colour.COLOUR_LIGHTRED().."\">Error: Vehicle model "..model.." does not exist.</>")
+	end
 
-    if string.len(plate) < 0 or string.len(plate) > 13 then
-        return AddPlayerChat(player, "<span color=\""..colour.COLOUR_LIGHTRED().."\">Error: Plate lengths range from 1 - 13.</>")
-    end
+	if string.len(plate) < 0 or string.len(plate) > 13 then
+		return AddPlayerChat(player, "<span color=\""..colour.COLOUR_LIGHTRED().."\">Error: Plate lengths range from 1 - 13.</>")
+	end
 
-    local x, y, z = GetPlayerLocation(player)
-    local a = GetPlayerHeading(player)
-    
-    local vehicle = Vehicle_Create(model, plate, x, y, z, a)
-    if vehicle == false then
-        AddPlayerChat(player, "<span color=\""..colour.COLOUR_LIGHTRED().."\">Error: Vehicle "..model.." wasn't able to be created!</>")
-    else
-        AddPlayerChat(player, string.format("<span color=\"%s\">Server: </>Vehicle %d (ID: %d) created successfully!", colour.COLOUR_LIGHTRED(), model, vehicle))
-        Slap(player)
-    end
-    return
+	local x, y, z = GetPlayerLocation(player)
+	local a = GetPlayerHeading(player)
+	
+	local vehicle = Vehicle_Create(model, plate, x, y, z, a)
+	if vehicle == false then
+		AddPlayerChat(player, "<span color=\""..colour.COLOUR_LIGHTRED().."\">Error: Vehicle "..model.." wasn't able to be created!</>")
+	else
+		AddPlayerChat(player, string.format("<span color=\"%s\">Server: </>Vehicle %d (ID: %d) created successfully!", colour.COLOUR_LIGHTRED(), model, vehicle))
+		Slap(player)
+	end
+	return
 end
 AddCommand('acreatevehicle', cmd_acv)
 AddCommand('acv', cmd_acv)
 
 function cmd_aev(player, vehicle, ownerid)
-    if (PlayerData[player].admin < 2) then
-        return AddPlayerChat(player, "<span color=\""..colour.COLOUR_LIGHTRED().."\">Error: You don't have permission to use this command.</>")
-    end
+	if (PlayerData[player].admin < 2) then
+		return AddPlayerChat(player, "<span color=\""..colour.COLOUR_LIGHTRED().."\">Error: You don't have permission to use this command.</>")
+	end
 
-    vehicle = tonumber(vehicle)
-    ownerid = tonumber(ownerid)
+	vehicle = tonumber(vehicle)
+	ownerid = tonumber(ownerid)
 
-    if vehicle == nil or ownerid == nil then
-        return AddPlayerChat(player, "<span color=\""..colour.COLOUR_LIGHTRED().."\">Usage:</> /(ae)dit(v)ehicle <vehicle> <ownerid>")
-    end
+	if vehicle == nil or ownerid == nil then
+		return AddPlayerChat(player, "<span color=\""..colour.COLOUR_LIGHTRED().."\">Usage:</> /(ae)dit(v)ehicle <vehicle> <ownerid>")
+	end
 
-    VehicleData[vehicle].owner = tonumber(ownerid)
-    AddPlayerChat(player, 'vehicle owner set to '..ownerid)
+	VehicleData[vehicle].owner = tonumber(ownerid)
+	AddPlayerChat(player, 'vehicle owner set to '..ownerid)
 
-    print('Vehicle is now: '..VehicleData[vehicle].owner..', player owner ID is '..PlayerData[player].id)
+	print('Vehicle is now: '..VehicleData[vehicle].owner..', player owner ID is '..PlayerData[player].id)
 end
 AddCommand('aeditvehicle', cmd_aev)
 AddCommand('aev', cmd_aev)
 
 AddCommand("ahelp", function (player)
-    if (PlayerData[player].admin < 1) then
-        return AddPlayerChat(player, "<span color=\""..colour.COLOUR_LIGHTRED().."\">Error: You don't have permission to use this command.</vehicle>")
-    end
+	if (PlayerData[player].admin < 1) then
+		return AddPlayerChat(player, "<span color=\""..colour.COLOUR_LIGHTRED().."\">Error: You don't have permission to use this command.</>")
+	end
 
-    if PlayerData[player].admin > 0 then
-        AddPlayerChat(player, "<span color=\""..colour.COLOUR_LIGHTRED().."\">Level 1: </>/a")
-    end
-    if PlayerData[player].admin > 1 then
-        AddPlayerChat(player, "<span color=\""..colour.COLOUR_LIGHTRED().."\">Level 2: </>/av /astats")
-    end
-    if PlayerData[player].admin > 2 then
-        AddPlayerChat(player, "<span color=\""..colour.COLOUR_LIGHTRED().."\">Level 3: </>None")
-    end
-    if PlayerData[player].admin > 3 then
-        AddPlayerChat(player, "<span color=\""..colour.COLOUR_LIGHTRED().."\">Level 4: </>/acreatevehicle /aeditvehicle")
-    end
-    if PlayerData[player].admin > 4 then
-        AddPlayerChat(player, "<span color=\""..colour.COLOUR_LIGHTRED().."\">Level 5: </>/apos /asetadmin")
-    end
+	if PlayerData[player].admin > 0 then
+		AddPlayerChat(player, "<span color=\""..colour.COLOUR_LIGHTRED().."\">Level 1: </>/a /get /goto")
+	end
+	if PlayerData[player].admin > 1 then
+		AddPlayerChat(player, "<span color=\""..colour.COLOUR_LIGHTRED().."\">Level 2: </>/av /astats")
+	end
+	if PlayerData[player].admin > 2 then
+		AddPlayerChat(player, "<span color=\""..colour.COLOUR_LIGHTRED().."\">Level 3: </>None")
+	end
+	if PlayerData[player].admin > 3 then
+		AddPlayerChat(player, "<span color=\""..colour.COLOUR_LIGHTRED().."\">Level 4: </>/acreatevehicle /aeditvehicle")
+	end
+	if PlayerData[player].admin > 4 then
+		AddPlayerChat(player, "<span color=\""..colour.COLOUR_LIGHTRED().."\">Level 5: </>/apos /asetadmin")
+	end
 end)
