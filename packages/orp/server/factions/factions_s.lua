0d0abcf9c77a0c719166b0b205f87a1d693ba969
AliLogic <drfiniz@gmail.com>
2019-12-18 12:32:04 +0500

    add factions base

---
+++ packages/orp/server/factions/factions_s.lua
@@ -0,0 +1,12 @@
+local colour = ImportPackage('colours')
+
+FactionData = {}
+FactionRankData = {}
+DivisionData = {}
+DivisionRankData = {}
+
+AddEvent("OnPackageStart", function ()
+end)
+
+AddEvent("OnPackageEnd", function ()
+end)
