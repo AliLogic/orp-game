a679f1ff5cdb5f1351acdf7e1a4449a5179a670b
AliLogic <drfiniz@gmail.com>
2019-12-21 00:59:25 +0500

    set env in OnPlayerSpawn client side

--- packages/orp/client/env/env_c.lua
+++ packages/orp/client/env/env_c.lua
@@ -1,5 +1,17 @@
+local EnvHour
+local EnvWeather
+
 AddRemoteEvent("UpdateClientTime", function (hour, weather)
+
+	EnvHour = hour
+	EnvWeather = weather
 
 	SetTime(hour)
 	SetWeather(weather)
+end)
+
+AddEvent("OnPlayerSpawn", function()
+
+	SetTime(EnvHour)
+	SetWeather(EnvWeather)
 end)
