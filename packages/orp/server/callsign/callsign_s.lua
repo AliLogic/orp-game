d02d0b9741942326fcb8d8a222f923b0252dc388
bork <alexrahali3@gmail.com>
2019-12-23 19:00:57 +0000

    fix?

--- packages/orp/server/callsign/callsign_s.lua
+++ packages/orp/server/callsign/callsign_s.lua
@@ -30,7 +30,7 @@ AddCommand("callsign", function (player, callsign)
     end
 
     VehicleData[vehicle].callsign = CreateText3D(callsign, 10.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0)
-    SetText3DAttached(VehicleData[vehicle].callsign, ATTACH_VEHICLE, vehicle, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, wheel_frear_l)
+    SetText3DAttached(VehicleData[vehicle].callsign, ATTACH_VEHICLE, vehicle, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, wheel_rear_l)
     
     AddPlayerChat(player, "<span color=\""..colour.COLOUR_LIGHTRED().."\">Server:</> Callsign "..callsign.." attached!")
 end)
