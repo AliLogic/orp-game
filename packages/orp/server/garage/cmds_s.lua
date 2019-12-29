229637e47fe99f4628af83e95d7cf419039066d7
AliLogic <drfiniz@gmail.com>
2019-12-30 01:09:07 +0500

    fix more issues

--- packages/orp/server/garage/cmds_s.lua
+++ packages/orp/server/garage/cmds_s.lua
@@ -1,36 +1,38 @@
+local colour = ImportPackage('colours')
+
 -- /acreategarage /aeditgarage /adestroygarage
 
-function cmd_acg(player, price)
+local function cmd_acg(player, price)
 	if (PlayerData[player].admin < 4) then
 		return AddPlayerChat(player, "<span color=\""..colour.COLOUR_LIGHTRED().."\">Error: You don't have permission to use this command.</>")
-    end
-    
-    if price == nil then
-        return AddPlayerChat(player, "<span color=\""..colour.COLOUR_LIGHTRED().."\">Usage:</> /(ac)reate(v)ehicle <price>")
-    end
+	end
 
-    price = tonumber(price)
+	if price == nil then
+		return AddPlayerChat(player, "<span color=\""..colour.COLOUR_LIGHTRED().."\">Usage:</> /(ac)reate(v)ehicle <price>")
+	end
 
-    if price < 0 then
-        return AddPlayerChat(player, "<span color=\""..colour.COLOUR_LIGHTRED().."\">Error: Garage price cannot be below $0.</>")
-    end
+	price = tonumber(price)
 
-    local x, y, z = GetPlayerLocation(player)
-    local a = GetPlayerHeading(player)
+	if price < 0 then
+		return AddPlayerChat(player, "<span color=\""..colour.COLOUR_LIGHTRED().."\">Error: Garage price cannot be below $0.</>")
+	end
 
-    local garage = Garage_Create(price, x, y, z, a)
-    if garage > 0 then
-        return AddPlayerChat(player, "<span color=\""..colour.COLOUR_LIGHTRED().."\">Success:</> Garage ID "..garage.." created successfully!")
-    end
+	local x, y, z = GetPlayerLocation(player)
+	local a = GetPlayerHeading(player)
+
+	local garage = Garage_Create(price, x, y, z, a)
+	if garage > 0 then
+		return AddPlayerChat(player, "<span color=\""..colour.COLOUR_LIGHTRED().."\">Success:</> Garage ID "..garage.." created successfully!")
+	end
 end
 
-function cmd_aeg(player, prefix, ...)
+local function cmd_aeg(player, prefix, ...)
 	if (PlayerData[player].admin < 4) then
 		return AddPlayerChat(player, "<span color=\""..colour.COLOUR_LIGHTRED().."\">Error: You don't have permission to use this command.</>")
-    end
-    
-    if prefix == "name" then
-        
+	end
+
+	if prefix == "name" then
+	end
 end
 AddCommand("aeditgarage", cmd_aeg)
 AddCommand("aeg", cmd_aeg)
