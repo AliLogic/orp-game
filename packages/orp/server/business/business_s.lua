8c8c6a11d513a4930ebba60cf0d54488e1197bff
bork <alexrahali3@gmail.com>
2019-12-29 18:40:09 +0000

    garage core

--- packages/orp/server/business/business_s.lua
+++ packages/orp/server/business/business_s.lua
@@ -146,7 +146,7 @@ function OnBusinessLoaded(businessid)
     else
         local business = GetFreeBusinessId()
         if business == 0 then
-            print("A free business id wasn't able to be found? ("..#BusinessData.."/"..MAX_BUSINESSES..") business SQL ID "..businessid)
+            print("A free business id wasn't able to be found? ("..#BusinessData.."/"..MAX_BUSINESSES..") business SQL ID "..businessid..".")
         end
     
         -- type, enterable, price, message, dimension, ix iy iz ia, ex ey ez ea, mx my mz
--- packages/orp/server/garage/garage_s.lua
+++ packages/orp/server/garage/garage_s.lua
@@ -27,6 +27,7 @@ function CreateGarageData(garage)
     GarageData[garage].ownership_type = 0
 
     GarageData[garage].price = 0
+    GarageData[garage].dimension = 0
 
     GarageData[garage].ix = 0
     GarageData[garage].iy = 0
@@ -41,4 +42,125 @@ end
 
 function DestroyGarageData(garage)
     GarageData[garage] = nil
+end
+
+AddEvent("UnloadGarages", function () 
+    for i = 1, #GarageData, 1 do
+        Garage_Unload(i)
+    end
+end)
+
+AddEvent("LoadGarages", function () 
+    mysql_async_query(sql, "SELECT * FROM garages;", OnGarageLoad)
+end)
+
+function OnGarageLoad()
+    for i = 1, mariadb_get_row_count(), 1 do
+        Garage_Load(mariadb_get_value_name_int(i, "id"))
+    end
+end
+
+function Garage_Create(price, ...)
+    name = table.concat({...}, " ")
+    price = tonumber(price)
+
+    if price < 0 then
+        return false
+    end
+    
+    if string.len(name) < 0 or string.len(name) > 32 then
+        return false
+    end
+    
+    local index = GetFreeGarageId()
+    if index == 0 then
+        return false
+    end
+
+    local query = mariadb_prepare(sql, "INSERT INTO garages (name, price) VALUES ('?', ?);",
+        name, price    
+    )
+    mariadb_async_query(sql, query, OnGarageCreated, index, name, price)
+end
+
+function OnGarageCreated(i, name, price)
+    GarageData[i].id = mariadb_get_insert_id()
+
+    GarageData[i].name = name
+    GarageData[i].price = price
+end
+
+function Garage_Load(garageid)
+    local query = mariadb_prepare(sql, "SELECT * FROM garages WHERE id = ?;", garageid)
+    mariadb_async_query(sql, query, OnGarageLoaded, garageid)
+end
+
+function OnGarageLoaded(garageid)
+    if mariadb_get_row_count() == 0 then
+        print("Failed to load garage SQL ID "..garageid)
+    else
+        local garage = GetFreeBusinessId()
+        if garage == 0 then
+            print("A free garage id wasn't able to be found? ("..#GarageData.."/"..MAX_GARAGES..") garage SQL ID "..garageid..".")
+        end
+
+        GarageData[garage].id = garageid
+        GarageData[garage].name = mariadb_get_value_name(garageid, "name")
+
+        GarageData[garage].owner = mariadb_get_value_name_int(garageid, "owner")
+        GarageData[garage].ownership_type = mariadb_get_value_name_int(garageid, "ownership_type")
+
+        GarageData[garage].price = mariadb_get_value_name_int(garageid, "price")
+        GarageData[garage].dimension = mariadb_get_value_name_int(garageid, "dimension")
+
+        GarageData[garage].ix = mariadb_get_value_name_float(garageid, "ix")
+        GarageData[garage].iy = mariadb_get_value_name_float(garageid, "iy")
+        GarageData[garage].iz = mariadb_get_value_name_float(garageid, "iz")
+        GarageData[garage].ia = mariadb_get_value_name_float(garageid, "ia")
+
+        GarageData[garage].ex = mariadb_get_value_name_float(garageid, "ex")
+        GarageData[garage].ey = mariadb_get_value_name_float(garageid, "ey")
+        GarageData[garage].ez = mariadb_get_value_name_float(garageid, "ez")
+        GarageData[garage].ea = mariadb_get_value_name_float(garageid, "ea")
+
+    end
+end
+
+function Garage_Unload(garage)
+    local query = mariadb_prepare(sql, "UPDATE garages SET name = '?', owner = ?, ownership_type = ?, price = ?, dimension = ?, ix = '?', iy = '?', iz = '?', ia = '?', ex = '?', ey = '?', ez = '?', ea = '?' WHERE id = ?;",
+        GarageData[garage].name,
+        GarageData[garage].owner,
+        GarageData[garage].ownership_type,
+        GarageData[garage].price,
+        GarageData[garage].dimension,
+        GarageData[garage].ix,
+        GarageData[garage].iy,
+        GarageData[garage].iz,
+        GarageData[garage].ia,
+        GarageData[garage].ex,
+        GarageData[garage].ey,
+        GarageData[garage].ez,
+        GarageData[garage].ea,
+        GarageData[garage].id    
+    )
+    mariadb_async_query(sql, query, OnGarageUnloaded, garage)
+end
+
+function OnGarageUnloaded(garage)
+    if mariadb_get_affected_rows() == 0 then
+        print("Garage ID "..garage.." unload unsuccessful!")
+    else
+        print("Garage ID "..garage.." unload successful!")
+    end 
+end
+
+function Garage_Destroy(garage)
+    if GarageData[garage] == nil then
+        return false
+    end
+
+    local query = mariadb_prepare(sql, "DELETE FROM garages WHERE id = ?;", GarageData[garage].id)
+    mariadb_async_query(sql, query)
+
+    DestroyGarageData(garage)
 end
