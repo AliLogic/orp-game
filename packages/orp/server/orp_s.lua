2f46e2addcbbea1d872ff3eaee7a5ddade142b18
AliLogic <drfiniz@gmail.com>
2019-12-30 14:57:02 +0500

    fix more nametag script errors

--- packages/orp/server/orp_s.lua
+++ packages/orp/server/orp_s.lua
@@ -13,19 +13,19 @@ local colour = ImportPackage('colours')
 
 AddEvent("OnPlayerJoin", function (player)
 	SetPlayerDimension(player, player)
-    SetPlayerSpawnLocation(player, 170694.515625, 194947.453125, 1396.9643554688, 90.0)
+	SetPlayerSpawnLocation(player, 170694.515625, 194947.453125, 1396.9643554688, 90.0)
 end)
 
 AddEvent("OnPlayerSpawn", function(player)
-    if PlayerData[player] ~= nil then
-        if PlayerData[player].logged_in ~= false then
-            if PlayerData[player].label ~= nil then
-                if IsValidText3D(PlayerData[player].label) then
-                    DestroyText3D(PlayerData[player].label)
-                end
-            end
-        end
-    end
+	if PlayerData[player] ~= nil then
+		if PlayerData[player].logged_in ~= false then
+			if PlayerData[player].label ~= nil then
+				if IsValidText3D(PlayerData[player].label) then
+					DestroyText3D(PlayerData[player].label)
+				end
+			end
+		end
+	end
 end)
 
 AddEvent("OnPlayerDeath", function (player, instigator)
@@ -37,53 +37,58 @@ AddEvent("OnPlayerQuit", function (player)
 end)
 
 AddEvent("OnPlayerWeaponShot", function(player, weapon, hittype, hitid, hitX, hitY, hitZ, startX, startY, startY, normalX, normalY, normalZ)
-    if player ~= 0 then
-        if weapon == 21 then
-            if hittype == HIT_PLAYER then
-                if IsValidPlayer(hitid) then
+	if player ~= 0 then
+		if weapon == 21 then
+			if hittype == HIT_PLAYER then
+				if IsValidPlayer(hitid) then
 
-                    local x, y, z = GetPlayerLocation(player)
-                    AddPlayerChatRange(x, y, 1000.0, "<span color=\""..colour.COLOUR_PURPLE().."\">* "..GetPlayerName(hitid).." falls on the ground after being hit by "..GetPlayerName(player).."'s taser.</>")
+					local x, y, z = GetPlayerLocation(player)
+					AddPlayerChatRange(x, y, 1000.0, "<span color=\""..colour.COLOUR_PURPLE().."\">* "..GetPlayerName(hitid).." falls on the ground after being hit by "..GetPlayerName(player).."'s taser.</>")
 
-                    AddPlayerChat(player, "<span color=\""..colour.COLOUR_YELLOW().."\">-> You hit "..GetPlayerName(hitid).." with your taser!</>")
-                    AddPlayerChat(hitid, "<span color=\""..colour.COLOUR_YELLOW().."\"> -> You were just hit by a taser. 10,000 volts go through your body. </>")
+					AddPlayerChat(player, "<span color=\""..colour.COLOUR_YELLOW().."\">-> You hit "..GetPlayerName(hitid).." with your taser!</>")
+					AddPlayerChat(hitid, "<span color=\""..colour.COLOUR_YELLOW().."\"> -> You were just hit by a taser. 10,000 volts go through your body. </>")
 
-                    -- SetPlayerHeading(hitid, GetPlayerHeading(player) - 180)
-                    -- SetPlayerAnimation(hitid, "LAY10")
-                    SetPlayerRagdoll(hitid, true)
+					-- SetPlayerHeading(hitid, GetPlayerHeading(player) - 180)
+					-- SetPlayerAnimation(hitid, "LAY10")
+					SetPlayerRagdoll(hitid, true)
 
-                    CallRemoteEvent(hitid, "ToggleTaseEffect", true)
+					CallRemoteEvent(hitid, "ToggleTaseEffect", true)
 
-                    Delay(5 * 1000, function()
-                        CallRemoteEvent(hitid, "ToggleTaseEffect", false)
-                        -- SetPlayerHeading(hitid, GetPlayerHeading(player) - 180)
-                        -- SetPlayerAnimation(hitid, "PUSHUP_END")
-                        SetPlayerRagdoll(hitid, false)
-                    end)
-                end
-            end
-        end
-    end
+					Delay(5 * 1000, function()
+						CallRemoteEvent(hitid, "ToggleTaseEffect", false)
+						-- SetPlayerHeading(hitid, GetPlayerHeading(player) - 180)
+						-- SetPlayerAnimation(hitid, "PUSHUP_END")
+						SetPlayerRagdoll(hitid, false)
+					end)
+				end
+			end
+		end
+	end
 end)
 
 AddEvent("OnPlayerStreamIn", function (player, otherplayer)
-	if PlayerData[otherplayer].id ~= 0 then
-		SetPlayerName(otherplayer, PlayerData[otherplayer].firstname.." "..PlayerData[otherplayer].lastname)
+	if PlayerData[otherplayer] ~= nil then
+		if PlayerData[otherplayer].id ~= 0 then
+			SetPlayerName(otherplayer, PlayerData[otherplayer].firstname.." "..PlayerData[otherplayer].lastname)
+		end
 	end
 end)
 
 AddEvent("OnPlayerSpawn", function(playerid)
-	if PlayerData[playerid].id ~= 0 then
-		SetPlayerName(playerid, PlayerData[playerid].firstname.." "..PlayerData[playerid].lastname)
+
+	if PlayerData[playerid] ~= nil then -- This is a fix for an error that happens when you spawn for the first time in the server...
+		if PlayerData[playerid].id ~= 0 then
+			SetPlayerName(playerid, PlayerData[playerid].firstname.." "..PlayerData[playerid].lastname)
+		end
 	end
 end)
 
 function fexist(filename) return file_exists(filename) end
 function file_exists(filename)
-    local file = io.open(filename, "r")
-    if file then
-        file:close()
-        return true
-    end
-    return false
+	local file = io.open(filename, "r")
+	if file then
+		file:close()
+		return true
+	end
+	return false
 end
