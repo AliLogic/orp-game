44cb5d5f13bff1df1d3c0940b3a0af84f0f80429
AliLogic <drfiniz@gmail.com>
2020-01-03 21:50:41 +0500

    increase speed

--- packages/orp/server/speedcam/speedcam_s.lua
+++ packages/orp/server/speedcam/speedcam_s.lua
@@ -87,7 +87,7 @@ function Speedcam_Create(x, y, z, speed)
 	end
 
 	SpeedcamData[index].objectid = CreateObject(963, x, y, z)
-	SpeedcamData[index].text3d = CreateText3D("Speedcam ("..index..")\nSpeed: "..speed.." KM/H", 20, x, y, z + 100.0, 0.0, 0.0, 0.0)
+	SpeedcamData[index].text3d = CreateText3D("Speedcam ("..index..")\nSpeed: "..speed.." KM/H", 20, x, y, z + 120.0, 0.0, 0.0, 0.0)
 
 	SpeedcamData[index].timer = CreateTimer(OnSpeedcamTick, 1000, index)
 
@@ -143,7 +143,7 @@ local function OnSpeedcamLoaded(speedcamid)
 		SpeedcamData[index].speed = mariadb_get_value_name_int(speedcamid, "speed")
 
 		SpeedcamData[index].objectid = CreateObject(963, SpeedcamData[index].x, SpeedcamData[index].y, SpeedcamData[index].z)
-		SpeedcamData[index].text3d = CreateText3D("Speedcam ("..index..")\nSpeed: "..SpeedcamData[index].speed.." KM/H", 20, SpeedcamData[index].x, SpeedcamData[index].y, SpeedcamData[index].z + 100.0, 0.0, 0.0, 0.0)
+		SpeedcamData[index].text3d = CreateText3D("Speedcam ("..index..")\nSpeed: "..SpeedcamData[index].speed.." KM/H", 20, SpeedcamData[index].x, SpeedcamData[index].y, SpeedcamData[index].z + 120.0, 0.0, 0.0, 0.0)
 
 		SpeedcamData[index].timer = CreateTimer(OnSpeedcamTick, 1000, index)
 	end
