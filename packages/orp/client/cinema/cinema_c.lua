22d13a8bcb666d34fdbfe7ee9b22ddff6aaa8e66
AliLogic <drfiniz@gmail.com>
2020-01-04 01:20:32 +0500

    another change

--- packages/orp/client/cinema/cinema_c.lua
+++ packages/orp/client/cinema/cinema_c.lua
@@ -24,7 +24,7 @@ end)
 AddRemoteEvent("StartImg", function ()
 	-- 123652, 96768, 1671
 	-- 123660, 96737, 1674
-	imgUI = CreateWebUI3D(123650, 96747, 1689, 0, 125, 0, 592, 310, 1)
+	imgUI = CreateWebUI3D(123670, 96750, 1689, 0, 125, 0, 592, 310, 1)
 	LoadWebFile(imgUI, "http://asset/"..GetPackageName().."/client/OnsetLogo.png")
 	SetWebAlignment(cinemaUI, 0.0, 0.0)
 	SetWebAnchors(cinemaUI, 0.0, 0.0, 1.0, 1.0)
