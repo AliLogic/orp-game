f61ba6af420bf6d3fb2afe1916a26db486fe32df
AliLogic <drfiniz@gmail.com>
2020-01-03 21:33:07 +0500

    cinema fixes

--- packages/orp/client/cinema/cinema_c.lua
+++ packages/orp/client/cinema/cinema_c.lua
@@ -14,19 +14,20 @@ local cinemaUI = 0
 local imgUI = 0
 
 AddRemoteEvent("StartMovie", function(link)
-    cinemaUI = CreateRemoteWebUI3D(173465, 198904, 2900, 0, 0, 0, 1920, 1080 , 30)
-    LoadWebFile(cinemaUI, link)
-    SetWebAlignment(cinemaUI, 0.0, 0.0)
-    SetWebAnchors(cinemaUI, 0.0, 0.0, 1.0, 1.0)
+	cinemaUI = CreateWebUI3D(173465, 198904, 2900, 0, 0, 0, 1920, 1080 , 30)
+	LoadWebFile(cinemaUI, link)
+	SetWebAlignment(cinemaUI, 0.0, 0.0)
+	SetWebAnchors(cinemaUI, 0.0, 0.0, 1.0, 1.0)
 	SetWebVisibility(cinemaUI, WEB_VISIBLE)
 end)
 
 AddRemoteEvent("StartImg", function ()
-    -- 123652, 96768, 1671
-    imgUI = CreateWebUI3D(123660, 96737, 1674, 0, 125, 0, 592, 330, 1)
-    LoadWebFile(imgUI, "http://asset/"..GetPackageName().."/client/OnsetLogo.png")
-    SetWebAlignment(cinemaUI, 0.0, 0.0)
-    SetWebAnchors(cinemaUI, 0.0, 0.0, 1.0, 1.0)
+	-- 123652, 96768, 1671
+	-- 123660, 96737, 1674
+	imgUI = CreateWebUI3D(123627, 96728, 1715, 0, 125, 0, 592, 330, 1)
+	LoadWebFile(imgUI, "http://asset/"..GetPackageName().."/client/OnsetLogo.png")
+	SetWebAlignment(cinemaUI, 0.0, 0.0)
+	SetWebAnchors(cinemaUI, 0.0, 0.0, 1.0, 1.0)
 	SetWebVisibility(cinemaUI, WEB_VISIBLE)
 end)
 
