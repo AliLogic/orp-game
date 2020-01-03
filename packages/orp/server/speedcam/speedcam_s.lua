256bad22ebe6d531be2a1c618acf64da4bd29497
bork <alexrahali3@gmail.com>
2020-01-03 18:40:01 +0000

    banking fix

--- packages/orp/server/speedcam/speedcam_s.lua
+++ packages/orp/server/speedcam/speedcam_s.lua
@@ -142,7 +142,7 @@ local function OnSpeedcamLoaded(speedcamid)
 		SpeedcamData[index].z = mariadb_get_value_name_int(speedcamid, "z")
 		SpeedcamData[index].speed = mariadb_get_value_name_int(speedcamid, "speed")
 
-		SpeedcamData[index].objectid = CreateObject(963, SpeedcamData[index].x, SpeedcamData[index].y, SpeedcamData[index].z)
+		SpeedcamData[index].objectid = CreateObject(963, SpeedcamData[index].x, SpeedcamData[index].y, SpeedcamData[index].z)		
 		SpeedcamData[index].text3d = CreateText3D("Speedcam ("..index..")\nSpeed: "..SpeedcamData[index].speed.." KM/H", 20, SpeedcamData[index].x, SpeedcamData[index].y, SpeedcamData[index].z + 120.0, 0.0, 0.0, 0.0)
 
 		SpeedcamData[index].timer = CreateTimer(OnSpeedcamTick, 1000, index)
