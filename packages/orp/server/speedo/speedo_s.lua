df1f94f7cfe7f2d0f29a75338b0f40ef2f69a9fb
bork <alexrahali3@gmail.com>
2020-01-04 15:16:41 +0000

    basic speedo

---
+++ packages/orp/server/speedo/speedo_s.lua
@@ -0,0 +1,7 @@
+AddRemoteEvent("speedo:IsPlayerInDriverSeat", function (player) 
+    if GetPlayerVehicleSeat(player) == 1 then
+        CallRemoteEvent(player, "speedo:ServerResponse", true)
+    else
+        CallRemoteEvent(player, "speedo:ServerResponse", false)
+    end
+end)
