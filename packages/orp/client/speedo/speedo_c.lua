fe8494837bdda4dddb10a6bb09e6277d6849ac2d
bork <alexrahali3@gmail.com>
2020-01-04 15:21:38 +0000

    indentation fix

--- packages/orp/client/speedo/speedo_c.lua
+++ packages/orp/client/speedo/speedo_c.lua
@@ -1,33 +1,33 @@
 local speedo = nil
 
 CreateTimer(function () 
-    if IsPlayerInVehicle() then
-        CallRemoteEvent("speedo:IsPlayerInDriverSeat")
-    end
+	if IsPlayerInVehicle() then
+		CallRemoteEvent("speedo:IsPlayerInDriverSeat")
+	end
 end, 500)
 
 AddRemoteEvent("speedo:ServerResponse", function (status) 
-    if status then
-        local speed = GetVehicleForwardSpeed(GetPlayerVehicle())
-        if speedo == nil then
-            if speed < 0 then
-                speed = speed * -1
-            end
+	if status then
+		local speed = GetVehicleForwardSpeed(GetPlayerVehicle())
+		if speedo == nil then
+			if speed < 0 then
+				speed = speed * -1
+			end
 
-            speedo = CreateTextBox(0, 0, speed.." KMH", "right")
-            SetTextBoxAnchors(speedo, 0.5, 0.5, 0.5, 0.5)
-            SetTextBoxAlignment(speedo, 0.5, 0.5)
-        else
-            if speed < 0 then
-                speed = speed * -1
-            end
+			speedo = CreateTextBox(0, 0, speed.." KMH", "right")
+			SetTextBoxAnchors(speedo, 0.5, 0.5, 0.5, 0.5)
+			SetTextBoxAlignment(speedo, 0.5, 0.5)
+		else
+			if speed < 0 then
+				speed = speed * -1
+			end
 
-            SetTextBoxText(speedo, speed.." KMH")
-        end
-    end
+			SetTextBoxText(speedo, speed.." KMH")
+		end
+	end
 end)
 
 AddEvent("OnPlayerLeaveVehicle", function(player, vehicle, seat)
-    DestroyTextBox(speedo)
-    speedo = nil
+	DestroyTextBox(speedo)
+	speedo = nil
 end)
