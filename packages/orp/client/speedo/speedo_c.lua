be6fe337806eab1c56fc48624436420ea879d0e8
AliLogic <drfiniz@gmail.com>
2020-01-04 20:49:54 +0500

    Update speedometer client side

--- packages/orp/client/speedo/speedo_c.lua
+++ packages/orp/client/speedo/speedo_c.lua
@@ -5,16 +5,18 @@ local function UpdateSpeedo()
 	local speed = GetPlayerVehicleSpeed()
 
 	if speedo == nil then
-		speedo = CreateTextBox(0, 0, speed.." KMH", "right")
+		speedo = CreateTextBox(0, -50, "<span color=\"#FF0000\" size=\"28\">"..speed.." KMH</>", "right")
 		SetTextBoxAnchors(speedo, 0.5, 0.5, 0.5, 0.5)
 		SetTextBoxAlignment(speedo, 0.5, 0.5)
 	end
 
-	if speed < 0 then
-		speed = speed * -1
-		SetTextBoxText(speedo, speed.." KMH (R)")
-	else
-		SetTextBoxText(speedo, speed.." KMH")
+	if speed ~= false then
+		if speed < 0 then
+			speed = speed * -1
+			SetTextBoxText(speedo, "<span color=\"#FF0000\" size=\"28\">"..speed.." KMH (R)")
+		else
+			SetTextBoxText(speedo, "<span color=\"#FF0000\" size=\"28\">"..speed.." KMH")
+		end
 	end
 end
 
@@ -30,11 +32,17 @@ AddRemoteEvent("ToggleSpeedo", function (bToggle)
 			DestroyTimer(timer)
 		end
 
-		DestroyTextBox(speedo)
+		if speedo ~= nil then
+			DestroyTextBox(speedo)
+			speedo = nil
+		end
 	end
 end)
 
 AddEvent("OnPlayerLeaveVehicle", function(player, vehicle, seat)
-	DestroyTextBox(speedo)
-	speedo = nil
+
+	if speedo ~= nil then
+		DestroyTextBox(speedo)
+		speedo = nil
+	end
 end)
