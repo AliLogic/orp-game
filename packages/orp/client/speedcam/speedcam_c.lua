23cbd581dbef8a6c1a7eeca44016d6f956ad18eb
AliLogic <drfiniz@gmail.com>
2020-01-04 15:50:11 +0500

    speedcam change

--- packages/orp/client/speedcam/speedcam_c.lua
+++ packages/orp/client/speedcam/speedcam_c.lua
@@ -1,21 +1,23 @@
-local speedcamFlash = 0
-local speedcamTimer = 0
+-- local speedcamFlash = 0
+-- local speedcamTimer = 0
 
 AddRemoteEvent("FlashSpeedcam", function()
 
-	if speedcamFlash ~= 0 then
-		speedcamFlash = CreateWebUI(0.0, 0.0, 1920, 1080, 1, 1)
-		SetWebURL(speedcamFlash, "https://www.solidbackgrounds.com/images/3600x3600/3600x3600-white-solid-color-background.jpg")
-	end
+	StartCameraFade(1, 0, 2, "#FFFFFF")
 
-	if speedcamTimer ~= 0 then
-		DestroyTimer(speedcamFlash)
-	end
+	-- if speedcamFlash ~= 0 then
+	-- 	speedcamFlash = CreateWebUI(0.0, 0.0, 1920, 1080, 1, 1)
+	-- 	SetWebURL(speedcamFlash, "https://www.solidbackgrounds.com/images/3600x3600/3600x3600-white-solid-color-background.jpg")
+	-- end
 
-	SetWebVisibility(speedcamFlash, WEB_HITINVISIBLE)
+	-- if speedcamTimer ~= 0 then
+	-- 	DestroyTimer(speedcamFlash)
+	-- end
 
-	speedcamFlash = CreateTimer(function()
-		SetWebVisibility(speedcamFlash, WEB_HIDDEN)
-		DestroyTimer(speedcamFlash)
-	end, 4000)
+	-- SetWebVisibility(speedcamFlash, WEB_HITINVISIBLE)
+
+	-- speedcamFlash = CreateTimer(function()
+	-- 	SetWebVisibility(speedcamFlash, WEB_HIDDEN)
+	-- 	DestroyTimer(speedcamFlash)
+	-- end, 4000)
 end)
