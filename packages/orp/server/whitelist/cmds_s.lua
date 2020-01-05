c1c52baff53f9b94c26c96ac53e2592e9cb971ca
AliLogic <drfiniz@gmail.com>
2020-01-05 19:13:16 +0500

    add whitelist scripts

---
+++ packages/orp/server/whitelist/cmds_s.lua
@@ -0,0 +1,39 @@
+AddCommand("whitelist", function(playerid, steam_id)
+	if (PlayerData[playerid].admin < 2) then
+		return AddPlayerChat(playerid, "<span color=\""..colour.COLOUR_LIGHTRED().."\">Error: You don't have permission to use this command.</>")
+	end
+
+	if (steam_id == nil or steam_id == 0) then
+		return AddPlayerChat(playerid, "Usage: /whitelist <steam id>")
+	end
+
+	if (#steam_id < 10 or #steam_id > 20) then
+		return AddPlayerChat(playerid, "Parameter \"steam id\" invalid length 10-20")
+	end
+
+	AddWhitelist(playerid, steam_id)
+	AddPlayerChat(playerid, "You have added whitelist to for account "..steam_id..".")
+end)
+
+AddCommand("whitelistlog", function(playerid, otherplayer)
+	if (PlayerData[playerid].admin < 1) then
+		return AddPlayerChat(playerid, "<span color=\""..colour.COLOUR_LIGHTRED().."\">Error: You don't have permission to use this command.</>")
+	end
+
+	local query = ""
+
+	if (otherplayer == nil) then
+		query = "SELECT a1.steam_name, a2.steam_name, FROM_UNIXTIME(log_whitelists.timestamp, '%a %H:%i') FROM log_whitelists INNER JOIN users a1 ON log_whitelists.id = a1.id INNER JOIN users a2 ON log_whitelists.admin_id = a2.id ORDER BY log_whitelists.time DESC LIMIT 10;"
+	else
+		otherplayer = math.tointeger(otherplayer)
+
+		if (not IsValidPlayer(otherplayer)) then
+			return AddPlayerChat(playerid, "Selected player does not exist")
+		end
+
+		query = mariadb_prepare(sql, "SELECT a1.steam_name, a2.steam_name, FROM_UNIXTIME(log_whitelists.timestamp, '%a %H:%i') FROM log_whitelists INNER JOIN users a1 ON log_whitelists.id = a1.id INNER JOIN users a2 ON log_whitelists.admin_id = a2.id WHERE a1.id = ? ORDER BY log_whitelists.time DESC LIMIT 10;",
+			PlayerData[otherplayer].accountid)
+	end
+
+	mariadb_async_query(sql, query, OnWhitelistLogLoaded, playerid)
+end)
---
+++ packages/orp/server/whitelist/whitelist_s.lua
@@ -0,0 +1,53 @@
+local colour = ImportPackage("colours")
+
+local function OnPlayerLoadWhitelist(player)
+	if mariadb_get_row_count() > 0 then
+
+		local allowed = mariadb_get_value_name_int(1, "allowed")
+
+		if allowed ~= 1 then
+			-- AddPlayerChat(player, "<span color=\"#FFFFFFFF\" style=\"normal\">Your account has been put off the</> <span color=\"#FF0000FF\" style=\"bold\">whitelist</> <span color=\"#FFFFFFFF\" style=\"normal\">. Visit the <span color=\"#FF0000FF\" style=\"bold\">roleplaystudios.co</> and contact the staff team.")
+			KickPlayer(player, "This account has been put off the whitelist.")
+		end
+	else
+		-- AddPlayerChat(player, "<span color=\"#FFFFFFFF\" style=\"normal\">Your account has not been</> <span color=\"#FF0000FF\" style=\"bold\">whitelisted</><span color=\"#FFFFFFFF\" style=\"normal\"> yet.</> Visit the <span color=\"#FF0000FF\" style=\"bold\">roleplaystudios.co</> to apply.")
+		KickPlayer(player, "This account is not whitelisted.")
+	end
+end
+
+function AddWhitelist(playerid, steam_id)
+	local query = mariadb_prepare(sql, "INSERT INTO whitelists (steam_id) VALUES ('?')", tostring(steam_id))
+	mariadb_async_query(sql, query)
+
+	query = mariadb_prepare(sql, "INSERT INTO log_whitelists (id, steam_id, timestamp) VALUES (?, '?', UNIX_TIMESTAMP())",
+		PlayerData[playerid].accountid, tostring(steam_id))
+	mariadb_async_query(sql, query)
+end
+
+function OnWhitelistLogLoaded(playerid)
+	if mariadb_get_row_count() == 0 then
+		return AddPlayerChat(playerid, "No whitelists logged")
+	end
+
+	local messages = ""
+
+	for i = 1, mariadb_get_row_count() do
+		local player_name = mariadb_get_value_name_int(i, "admin_id")
+		local admin_name = mariadb_get_value_name(i, "steam_id")
+		local time = mariadb_get_value_name(i, "timestamp")
+
+		messages = messages.."("..time..") "..admin_name.." whitelisted "..player_name.."<br>"
+	end
+
+	-- webgui.ShowMessageBox(playerid, messages)
+end
+
+AddEvent("OnPlayerSteamAuth", function(playerid)
+
+	local query = mariadb_prepare(sql, "SELECT * FROM whitelists WHERE steam_id = '?' LIMIT 1;",
+		tostring(GetPlayerSteamId(playerid)))
+	mariadb_query(sql, query, OnPlayerLoadWhitelist, playerid)
+end)
+
+--
+-- END OF THE FILE
