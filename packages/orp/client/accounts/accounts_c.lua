d93847e05b36b789ed5888808a21a81088cbad1e
AliLogic <drfiniz@gmail.com>
2020-01-07 00:00:19 +0500

    fix web visibility enum

--- packages/orp/client/accounts/accounts_c.lua
+++ packages/orp/client/accounts/accounts_c.lua
@@ -181,7 +181,7 @@ AddEvent('charui:spawn', function (slot)
 	SetIgnoreMoveInput(false)
 	ShowMouseCursor(false)
 	SetInputMode(INPUT_GAME)
-    SetWebVisibility(charUI, WEB_INVISIBLE)
+    SetWebVisibility(charUI, WEB_HIDDEN)
     DestroyWebUI(charUI)
 
     count = 0
