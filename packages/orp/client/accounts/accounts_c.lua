4510119963b0aaeab80ec81fe5f9ef7cccb5c26d
AliLogic <drfiniz@gmail.com>
2020-01-06 15:29:38 +0500

    character selection UI cleanup and fixes

--- packages/orp/client/accounts/accounts_c.lua
+++ packages/orp/client/accounts/accounts_c.lua
@@ -1,15 +1,15 @@
 local Dialog = ImportPackage("dialogui")
 Dialog.setGlobalTheme("default-dark")
 
-local charcreate = Dialog.create("New character:", "Create a new character", "Create", "Quit")
-Dialog.setAutoclose(charcreate, false)
-Dialog.addTextInput(charcreate, 1, "First Name:")
-Dialog.addTextInput(charcreate, 1, "Last Name:")
-Dialog.addSelect(charcreate, 1, "Gender:", 1, "Male", "Female")
+local charCreate = Dialog.create("New character:", "Create a new character", "Create", "Quit")
+Dialog.setAutoclose(charCreate, false)
+Dialog.addTextInput(charCreate, 1, "First Name:")
+Dialog.addTextInput(charCreate, 1, "Last Name:")
+Dialog.addSelect(charCreate, 1, "Gender:", 1, "Male", "Female")
 
---[[local charviewnone = Dialog.create("Select character:", "Select a character to spawn as. If slot is empty, you will be prompted to create a new character.", "Select", "Quit")
-Dialog.setAutoclose(charviewnone, false)
-Dialog.addSelect(charviewnone, 1, "Select a character below:", 1, "(1) Create New Character", "(2) Create New Character", "(3) Create New Character")]]
+local charViewNone = Dialog.create("Select character:", "Select a character to spawn as. If slot is empty, you will be prompted to create a new character.", "Select", "Quit")
+Dialog.setAutoclose(charViewNone, false)
+Dialog.addSelect(charViewNone, 1, "Select a character below:", 1, "(1) Create New Character", "(2) Create New Character", "(3) Create New Character")
 
 local charview = nil
 local creation_slot = 0
@@ -23,10 +23,10 @@ SetWebVisibility(charUI, WEB_HIDDEN)
 local charUIready = false
 
 AddEvent("OnWebLoadComplete", function(web)
-    if web == charUI then
-        AddPlayerChat("WebUI now ready 1")
-        charUIready = true
-    end
+	if web == charUI then
+		AddPlayerChat("WebUI now ready 1")
+		charUIready = true
+	end
 end)
 
 AddEvent("OnPackageStop", function ()
@@ -34,158 +34,160 @@ AddEvent("OnPackageStop", function ()
 end)
 
 AddRemoteEvent("askClientCreation", function ()
-    Dialog.show(charcreate)
+	Dialog.show(charCreate)
 end)
 
 AddRemoteEvent("askClientShowCharSelection", function(chardata)    
-    SetWebVisibility(charUI, WEB_VISIBLE)
+	SetWebVisibility(charUI, WEB_VISIBLE)
+	SetInputMode(charUI, INPUT_UI)
+	ShowMouseCursor(true)
 
-    if chardata == nil then
-        ExecuteWebJS(charUI, "toggleCharMenu();")
-        return
-    end
+	if chardata == nil then
+		ExecuteWebJS(charUI, "toggleCharMenu();")
+		return
+	end
 
-    local count = 0
-    for _ in pairs(chardata) do count = count + 1 end
-    
-    if count ~= 0 then
-        for i = 1, count, 1 do
-            AddPlayerChat(string.format("setCharacterInfo({slot:%d,firstname:\"%s\",lastname:\"%s\",level:%d,cash:%d});",
-            i, chardata[i].firstname, chardata[i].lastname, chardata[i].level, chardata[i].cash
-            ))
-            ExecuteWebJS(charUI, "setCharacterInfo({slot:"..i..",firstname:"..chardata[i].firstname..",lastname:"..chardata[i].lastname..",level:"..chardata[i].level..",cash:"..chardata[i].cash.."});")
-        end
-    end
+	local count = 0
+	for _ in pairs(chardata) do count = count + 1 end
 
-    ExecuteWebJS(charUI, "test();")
-    ExecuteWebJS(charUI, "toggleCharMenu();")
-    
-    --[[if count == 0 then
-        AddPlayerChat("Count is 0")
-        Dialog.show(charviewnone)
-    else
-        local char = {}
-        char[1] = 'Create New Character'
-        char[2] = 'Create New Character'
-        char[3] = 'Create New Character'
+	if count ~= 0 then
+		for i = 1, count, 1 do
+			AddPlayerChat(string.format("setCharacterInfo({slot:%d,firstname:\"%s\",lastname:\"%s\",level:%d,cash:%d});",
+			i, chardata[i].firstname, chardata[i].lastname, chardata[i].level, chardata[i].cash
+			))
+			ExecuteWebJS(charUI, "setCharacterInfo({slot:"..i..",firstname:"..chardata[i].firstname..",lastname:"..chardata[i].lastname..",level:"..chardata[i].level..",cash:"..chardata[i].cash.."});")
+		end
+	end
 
-        for i = 1, count, 1 do
-            char[i] = chardata[i].firstname.." "..chardata[i].lastname
-        end
+	ExecuteWebJS(charUI, "test();")
+	ExecuteWebJS(charUI, "toggleCharMenu();")
 
-        charview = Dialog.create("Select character:", "Select a character to spawn as. If slot is empty, you will be prompted to create a new character.", "Select", "Quit")
-        Dialog.setAutoclose(charview, false)
-        Dialog.addSelect(charview, 1, "Select a character below:", 1, "(1) "..char[1], "(2) "..char[2], "(3) "..char[3])
-        Dialog.show(charview)
-    end]]
+	--[[if count == 0 then
+		AddPlayerChat("Count is 0")
+		Dialog.show(charViewNone)
+	else
+		local char = {}
+		char[1] = 'Create New Character'
+		char[2] = 'Create New Character'
+		char[3] = 'Create New Character'
+
+		for i = 1, count, 1 do
+			char[i] = chardata[i].firstname.." "..chardata[i].lastname
+		end
+
+		charview = Dialog.create("Select character:", "Select a character to spawn as. If slot is empty, you will be prompted to create a new character.", "Select", "Quit")
+		Dialog.setAutoclose(charview, false)
+		Dialog.addSelect(charview, 1, "Select a character below:", 1, "(1) "..char[1], "(2) "..char[2], "(3) "..char[3])
+		Dialog.show(charview)
+	end]]
 end)
 
 AddEvent("OnDialogSubmit", function(dialog, button, firstname, lastname, gender)
-    if dialog == charcreate then
-        if button == 1 then
-            if string.len(firstname) < 1 then
-                return AddPlayerChat('You must enter in a username!')
-            end
-    
-            if string.len(lastname) < 1 then
-                return AddPlayerChat('You must enter in a lastname!')
-            end
-    
-            if string.match(firstname, "Create") or string.match(lastname, "Create") then
-                return AddPlayerChat('Blacklisted word in your first or lastname, please verify you\'ve entered in a valid name.')
-            end
-                
-            if string.match(firstname, '[A-Z][a-z]*') == nil or string.match(lastname, '[A-Z][a-z]*') == nil then
-                return AddPlayerChat('Your Firstname and Lastname must begin with a capital letter, followed by lowercase letter (Example: John, Doe).')
-            end
+	if dialog == charCreate then
+		if button == 1 then
+			if string.len(firstname) < 1 then
+				return AddPlayerChat('You must enter in a username!')
+			end
 
-            if string.len(string.format("%s %s", firstname, lastname)) > 32 then
-                return AddPlayerChat('Your firstname or lastname is too long.')
-            end
-    
-            Dialog.close(charcreate)
-            CallRemoteEvent("accounts:characterCreated", string.match(firstname, '[A-Z][a-z]*'), string.match(lastname, '[A-Z][a-z]*'), gender)
-        else
-            Dialog.close(charcreate)
-            CallRemoteEvent("accounts:kick")
-        end
-    elseif dialog == charviewnone then
-        if button == 1 then
-            creation_slot = 1
-            Dialog.close(charviewnone)
-            Dialog.show(charcreate)
-        else
-            Dialog.close(charviewnone)
-            CallRemoteEvent("accounts:kick")
-        end
-    --[[elseif dialog == charview then
-        if button == 1 then
-            local slot = firstname
+			if string.len(lastname) < 1 then
+				return AddPlayerChat('You must enter in a lastname!')
+			end
 
-            if string.match(slot, "Create") then
-                creation_slot = math.tointeger(string.match(slot, "%d"))
-                Dialog.close(charview)
-                Dialog.show(charcreate)
-                return
-            end
+			if string.match(firstname, "Create") or string.match(lastname, "Create") then
+				return AddPlayerChat('Blacklisted word in your first or lastname, please verify you\'ve entered in a valid name.')
+			end
 
-            AddPlayerChat('Logging in as '..string.match(slot, '[a-zA-Z]+ [a-zA-Z]+'))
-            CallRemoteEvent("accounts:login", math.tointeger(string.match(slot, "%d")))
-            Dialog.close(charview)
-        else
-            Dialog.close(charview)
-            CallRemoteEvent("accounts:kick")
-        end]]
-    else
-        return
-    end
+			if string.match(firstname, '[A-Z][a-z]*') == nil or string.match(lastname, '[A-Z][a-z]*') == nil then
+				return AddPlayerChat('Your Firstname and Lastname must begin with a capital letter, followed by lowercase letter (Example: John, Doe).')
+			end
+
+			if string.len(string.format("%s %s", firstname, lastname)) > 32 then
+				return AddPlayerChat('Your firstname or lastname is too long.')
+			end
+
+			Dialog.close(charCreate)
+			CallRemoteEvent("accounts:characterCreated", string.match(firstname, '[A-Z][a-z]*'), string.match(lastname, '[A-Z][a-z]*'), gender)
+		else
+			Dialog.close(charCreate)
+			CallRemoteEvent("accounts:kick")
+		end
+	elseif dialog == charViewNone then
+		if button == 1 then
+			creation_slot = 1
+			Dialog.close(charViewNone)
+			Dialog.show(charCreate)
+		else
+			Dialog.close(charViewNone)
+			CallRemoteEvent("accounts:kick")
+		end
+	--[[elseif dialog == charview then
+		if button == 1 then
+			local slot = firstname
+
+			if string.match(slot, "Create") then
+				creation_slot = math.tointeger(string.match(slot, "%d"))
+				Dialog.close(charview)
+				Dialog.show(charCreate)
+				return
+			end
+
+			AddPlayerChat('Logging in as '..string.match(slot, '[a-zA-Z]+ [a-zA-Z]+'))
+			CallRemoteEvent("accounts:login", math.tointeger(string.match(slot, "%d")))
+			Dialog.close(charview)
+		else
+			Dialog.close(charview)
+			CallRemoteEvent("accounts:kick")
+		end]]
+	else
+		return
+	end
 end)
 
 --[[            ExecuteWebJS(web, "toggleCharMenu();");
-            SetIgnoreLookInput(false)
-            SetIgnoreMoveInput(false)
-            ShowMouseCursor(false)
-            SetInputMode(INPUT_GAME)
-            SetWebVisibility(web, WEB_HITINVISIBLE)
-            ]]
+			SetIgnoreLookInput(false)
+			SetIgnoreMoveInput(false)
+			ShowMouseCursor(false)
+			SetInputMode(INPUT_GAME)
+			SetWebVisibility(web, WEB_HITINVISIBLE)
+			]]
 
 AddEvent('charui:create', function (slot)
-    ExecuteWebJS(charUI, "toggleCharMenu();");
-    SetIgnoreLookInput(false)
-    SetIgnoreMoveInput(false)
-    ShowMouseCursor(false)
-    SetInputMode(INPUT_GAME)
-    SetWebVisibility(charUI, WEB_HITINVISIBLE)
+	ExecuteWebJS(charUI, "toggleCharMenu();");
+	SetIgnoreLookInput(false)
+	SetIgnoreMoveInput(false)
+	ShowMouseCursor(false)
+	SetInputMode(INPUT_GAME)
+	SetWebVisibility(charUI, WEB_HITINVISIBLE)
 
-    creation_slot = math.tointeger(slot)
-    Dialog.show(charcreate)
+	creation_slot = math.tointeger(slot)
+	Dialog.show(charCreate)
 end)
 
 AddEvent('charui:spawn', function (slot)
-    ExecuteWebJS(charUI, "toggleCharMenu();");
-    SetIgnoreLookInput(false)
-    SetIgnoreMoveInput(false)
-    ShowMouseCursor(false)
-    SetInputMode(INPUT_GAME)
-    SetWebVisibility(charUI, WEB_HITINVISIBLE)
+	ExecuteWebJS(charUI, "toggleCharMenu();");
+	SetIgnoreLookInput(false)
+	SetIgnoreMoveInput(false)
+	ShowMouseCursor(false)
+	SetInputMode(INPUT_GAME)
+	SetWebVisibility(charUI, WEB_HITINVISIBLE)
 
-    AddPlayerChat('Logging in as '..name)
-    CallRemoteEvent("accounts:login", math.tointeger(slot))
+	AddPlayerChat('Logging in as '..GetPlayerName())
+	CallRemoteEvent("accounts:login", math.tointeger(slot))
 end)
 
 AddEvent('charui:debug', function (text) 
-    AddPlayerChat(text)
+	AddPlayerChat(text)
 end)
 
 --[[AddRemoteEvent('FreezePlayer', function (player)
-    if PlayerData[player].is_frozen == false then
-        SetIgnoreMoveInput(false)
-        SetIgnoreLookInput(false)
-        PlayerData[player].is_frozen = true
-    else
-        SetIgnoreMoveInput(true)
-        SetIgnoreLookInput(true)
-        PlayerData[player].is_frozen = false
-    end
-    return true
+	if PlayerData[player].is_frozen == false then
+		SetIgnoreMoveInput(false)
+		SetIgnoreLookInput(false)
+		PlayerData[player].is_frozen = true
+	else
+		SetIgnoreMoveInput(true)
+		SetIgnoreLookInput(true)
+		PlayerData[player].is_frozen = false
+	end
+	return true
 end)]]--
