1dc92531f4c962b93f798038704efcfaaad21b84
AliLogic <drfiniz@gmail.com>
2020-01-12 23:55:00 +0500

    female char sys test

--- packages/orp/client/models/femalechar/femalechar_c.lua
+++ packages/orp/client/models/femalechar/femalechar_c.lua
@@ -2,15 +2,15 @@ AddEvent("OnPackageStart", function ()
 	local pakname = "FemaleChar"
 	local res = LoadPak(pakname, "/FemaleChar/", "../../../OnsetModding/Plugins/FemaleChar/Content")
 	AddPlayerChat("Loading of "..pakname..": "..tostring(res))
-
-	for _, v in pairs(GetAllFilesInPak(pakname)) do
-		AddPlayerChat(v)
-	end
 end)
 
 function SetPlayerFemale(player)
     local SkeletalMeshComponent = GetPlayerSkeletalMeshComponent(player, "Body")
 	SkeletalMeshComponent:SetSkeletalMesh(USkeletalMesh.LoadFromAsset('/FemaleChar/FemaleChar'))
 	AddPlayerChat('Set to female')
+
+	for _, v in pairs(GetAllFilesInPak("FemaleChar")) do
+		AddPlayerChat(v)
+	end
 end
 AddRemoteEvent('SetPlayerFemale', SetPlayerFemale)
