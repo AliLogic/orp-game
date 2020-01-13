a61ecce1c063678a258c9bde6349dc1057d71cff
bork <alexrahali3@gmail.com>
2020-01-13 19:10:08 +0000

    borkui push

---
+++ packages/borkui/borkui.js
@@ -0,0 +1,281 @@
+let elements = [];
+let elementId = 0;
+
+$(document).ready(() => {
+	CallEvent('borkui:ready');
+
+	/*createUI(0);
+	addTitle('Bork UI');
+	addDivider();
+	addInformation('This is just a basic test.');
+	addDivider();
+	addTextInput('Name:');
+	addDropdown(['test of the century', 'test2']);
+	addButton('test', 'is-success');
+	showUI(1);*/
+});
+
+function createUI(align = 0) {
+	switch (align) {
+		case 1: {
+			$('.wrapper').css({'margin-left': '0%', 'margin-right': '66%'});
+			break;
+		}
+		case 2: {
+			$('.wrapper').css({'margin-left': '66%', 'margin-right': '0%'});
+			break;
+		}
+		default: {
+			$('.wrapper').css({'margin-left': '33%', 'margin-right': '33%'});
+			break;
+		}
+	}
+}
+
+function addTitle(text) {
+	$('#title').html(`<h1 class="title" id="title">${text}</h1>`);
+}
+
+function addInformation(text, anchor = 0) {
+	/* 
+		Anchors:
+		0: center
+		1: left
+		2: right
+	*/
+
+	let alignment;
+	switch (anchor) {
+		case 1: {
+			alignment = ' style="text-align: left;"';
+			break;
+		}
+		case 2: {
+			alignment = ' style="text-align: right;"';
+			break;
+		}
+		default: {
+			alignment = '';
+			break;
+		}
+	}
+
+	$('#card').append(`<div${alignment}><span>${text}</span></div>`);
+}
+
+function addDivider() {
+	$('#content').append('<div class="is-divider"></div>');
+}
+
+function addButton(text, colour = 'is-dark', size = 1, rounded = false, fullwidth = true, anchor = 0) {
+	/* 
+		Anchors:
+		0: center
+		1: left
+		2: right
+	*/
+
+	/* 
+		Sizes:
+		0 - Small
+		1 - Normal (Default)
+		2 - Medium
+		3 - Large
+	*/
+
+	let alignment;
+	let chosen_size;
+
+	switch (anchor) {
+		case 1: {
+			alignment = ' style="text-align: left;"';
+			break;
+		}
+		case 2: {
+			alignment = ' style="text-align: right;"';
+			break;
+		}
+		default: {
+			alignment = '';
+			break;
+		}
+	}
+
+	switch (size) {
+		case 0: {
+			chosen_size = 'is-small';
+			break;
+		}
+		case 2: {
+			chosen_size = 'is-medium';
+			break;
+		}
+		case 3: {
+			chosen_size = 'is-large';
+			break;
+		}
+		default: {
+			chosen_size = '';
+			break;
+		}
+	}
+
+	elementId += 1;
+	elements.push([elementId, true]);
+	
+	console.log('Button ID: '+ elementId);
+	
+	if (colour.startsWith('is-')) {
+		$('#content').append(`<div${alignment}><button class="button ${colour} ${chosen_size} ${fullwidth ? 'is-fullwidth' : ''} ${rounded ? 'is-rounded' : ''}" id="${elementId}">${text}</button></div>`)
+	} else {
+		$('#content').append(`<div${alignment}><button class="button ${chosen_size} ${fullwidth ? 'is-fullwidth' : ''} ${rounded ? 'is-rounded' : ''}" id="${elementId}" style="background-color: ${colour};">${text}</button></div>`)
+	}
+}
+
+function addTextInput(label, size = 1, type = 0, placeholder = '') {
+	/* 
+		Types:
+		0: text
+		1: password
+	*/
+
+	let chosen_size;
+	let chosen_type;
+	switch (size) {
+		case 0: {
+			chosen_size = 'is-small';
+			break;
+		}
+		case 2: {
+			chosen_size = 'is-medium';
+			break;
+		}
+		case 3: {
+			chosen_size = 'is-large';
+			break;
+		}
+		default: {
+			chosen_size = '';
+			break;
+		}
+	}
+
+	switch (type) {
+		case 1: {
+			chosen_type = 'password';
+			break;
+		}
+		default: {
+			chosen_type = 'text';
+			break;
+		}
+	}
+
+	elementId += 1;
+	elements.push([elementId, false]);
+
+	console.log('Text Input ID: '+ elementId);
+
+	if (placeholder !== '') placeholder = "placeholder=\""+placeholder+"\"";
+	$('#content').append(`<div class="field"><div class="control"><label class="label">${label}</label><input class="input ${chosen_size}" id="${elementId}" type="${chosen_type}" ${placeholder}></div></div>`);
+}
+
+function addDropdown(options, size = 1, rounded = false, label = '') {
+	let optionhtml = '';
+	let chosen_size;
+	switch (size) {
+		case 0: {
+			chosen_size = 'is-small';
+			break;
+		}
+		case 2: {
+			chosen_size = 'is-medium';
+			break;
+		}
+		case 3: {
+			chosen_size = 'is-large';
+			break;
+		}
+		default: {
+			chosen_size = '';
+			break;
+		}
+	}
+
+	options.forEach((option) => {
+		optionhtml += `<option>${option}</option>`;
+	});
+
+	elementId += 1;
+	elements.push([elementId, false]);
+
+	console.log('Dropdown ID: '+ elementId);
+
+	if (label.length > 0) {
+		label = `<label class="label">${label}</label>`;
+	}
+
+	$('#content').append(`<div class="field"><div class="control">${label}<div class="select ${chosen_size}"><select id="${elementId}">${optionhtml}</select></div></div></div>`);
+}
+
+/*function addButtonsSideBySide(...buttons) {
+	/*
+		Must be given:
+		text - Text to be displayed.
+		colour - Colour to set the button.
+
+	let html = '<div class="columns">';
+	let buttonshtml = [];
+
+	if (buttons.length <= 4) {
+		for (let button of buttons) {
+			if (button[1].startsWith('is-')) {
+				buttonshtml.push(`<div class="column"><button class="button ${button[1]} is-fullwidth" id="${buttonCount}">${button[0]}</button></div>`);
+			} else {
+				buttonshtml.push(`<div class="column"><button class="button is-fullwidth" id="${buttonCount}" style="background-color: ${button[0]};">${text}</button></div>`)
+			}
+		}
+
+		buttonshtml.forEach((button) => {
+			html += button;
+		});
+
+		html += '</div>'
+		$('#card').append(html);
+	}
+}*/
+
+const buttonHandler = function (e) {
+	e.preventDefault();
+	let returnValues = [id, parseInt($(this).attr('id')) - elements.length];
+
+	elements.forEach((element) => {
+		if (!element[1]) {
+			console.log($(`#${element[0]}`).val());
+			returnValues.push($(`#${element[0]}`).val());
+		}
+	});
+
+	console.log(returnValues);
+	CallEvent('borkui:OnDialogSubmit', returnValues);
+}
+
+function clearUI() {
+	$('#content').html('');
+	$('#title').html('');
+	CallEvent('borkui:OnHideMenu');
+}
+
+function showUI(id) {
+	if ($('body').is(':hidden')) {
+		$('body').show();
+		$('button').on('click', buttonHandler);
+	}
+}
+
+function hideUI(id) {
+	if ($('body').is(':visible')) {
+		$('body').hide();
+		$('button').off('click', buttonHandler);
+	}
+}
---
+++ packages/borkui/borkui_c.lua
@@ -0,0 +1,245 @@
+local web = CreateWebUI(0, 0, 0, 0, 1, 16)
+SetWebAlignment(web, 0, 0)
+SetWebAnchors(web, 0, 0, 1, 1)
+SetWebURL(web, "http://asset/"..GetPackageName().."/borkui.html")
+SetWebVisibility(web, WEB_HITINVISIBLE)
+
+local nextId = 1
+local dialogs = {}
+local lastOpened = 0
+
+local COLUMN_TYPE_DIVIDER = 1
+local COLUMN_TYPE_BUTTON = 2
+local COLUMN_TYPE_TEXTINPUT = 3
+local COLUMN_TYPE_DROPDOWN = 4
+
+AddEvent("OnPackageStop", function()
+	DestroyWebUI(web)
+end)
+
+function CreateUI(align)
+	if align < 0 or align > 2 then
+		align = 0
+	end
+
+	local id = nextId
+	nextId = nextId + 1
+
+	dialogs[id] = {
+		title = '',
+		columns = {}
+	}
+
+	return id
+end
+
+function AddUITitle(id, text)
+	if dialogs[id] == nil then
+		return false
+	end
+
+	if string.len(text) < 1 then
+		return false
+	end
+	dialogs[id].title = text
+end
+
+function AddUIInformation(id, text)
+	if dialogs[id] == nil then
+		return false
+	end
+
+	if string.len(text) < 1 then
+		return false
+	end
+
+	dialogs[id].info = text
+end
+
+function AddUIDivider(id)
+	if dialogs[id] == nil then
+		return false
+	end
+
+	table.insert(dialogs[id].columns, {COLUMN_TYPE_DIVIDER})
+end
+
+function AddUIButton(id, text, colour, size, rounded, fullwidth, anchor)
+	if dialogs[id] == nil then
+		return false
+	end
+
+	colour = colour or 'is-dark'
+	size = size or 1
+	rounded = rounded or false
+	fullwidth = fullwidth or true
+	anchor = anchor or 0
+
+	if anchor < 0 or anchor > 2 then
+		anchor = 0
+	end
+
+	if size < 0 or size > 3 then
+		size = 1
+	end
+
+	table.insert(dialogs[id].columns, {COLUMN_TYPE_BUTTON, text, colour, size, rounded, fullwidth, anchor})
+end
+
+function AddUITextInput(id, label, size, type, placeholder)
+	if dialogs[id] == nil then
+		return false
+	end
+
+	label = label or 'Default'
+	size = size or 1
+	type = type or 0
+	placeholder = placeholder or ''
+
+	if string.len(label) < 1 then
+		label = 'Default'
+	end
+
+	if size < 0 or size > 3 then
+		size = 1
+	end
+
+	if type < 0 or type > 1 then
+		type = 0
+	end
+
+	table.insert(dialogs[id].columns, {COLUMN_TYPE_TEXTINPUT, label, size, type, placeholder})
+end
+
+function AddUIDropdown(id, options, size, rounded, label)
+	if dialogs[id] == nil then
+		return false
+	end
+
+	size = size or 1
+	rounded = rounded or false
+	label = label or ''
+
+	if size < 0 or size > 3 then
+		size = 1
+	end
+
+	local jsonoptions = '['
+
+	for i = 1, #options, 1 do
+		if i == #options then
+			jsonoptions = jsonoptions.."'"..options[i].."'"
+		else
+			jsonoptions = jsonoptions.."'"..options[i].."'"..','
+		end
+	end
+	
+	jsonoptions = jsonoptions..']'
+
+	table.insert(dialogs[id].columns, {COLUMN_TYPE_DROPDOWN, jsonoptions, size, rounded, label})
+end
+
+function ShowUI(id)
+	if dialogs[id] == nil then
+		return false
+	end
+
+	if dialogs[id].title == '' then
+		return false
+	end
+
+	if #dialogs[id].columns < 1 then
+		return false
+	end
+
+	if lastOpened ~= 0 then
+		HideUI(lastOpened)
+	end
+
+	local column
+
+	for i = 1, #dialogs[id].columns, 1 do
+		column = dialogs[id].columns[i]
+
+		if dialogs[id].columns[i][1] == COLUMN_TYPE_DIVIDER then
+			ExecuteWebJS(web, 'addDivider();')
+		elseif dialogs[id].columns[i][1] == COLUMN_TYPE_BUTTON then
+			ExecuteWebJS(web, string.format('addButton("%s", "%s", %d, %s, %s, %d);', column[2], column[3], column[4], tostring(column[5]), tostring(column[6]), column[7]))
+		elseif dialogs[id].columns[i][1] == COLUMN_TYPE_TEXTINPUT then
+			ExecuteWebJS(web, string.format('addTextInput("%s", %d, %d, "%s");', column[2], column[3], column[4], column[5]))
+		elseif dialogs[id].columns[i][1] == COLUMN_TYPE_DROPDOWN then
+			ExecuteWebJS(web, string.format('addTextInput(%s, %d, %s, "%s");', column[2], column[3], tostring(column[4]), column[5]))
+		end
+	end
+
+	SetWebVisibility(web, WEB_VISIBLE)
+	ExecuteWebJS(web, 'showUI('..id..');')
+	SetIgnoreLookInput(true)
+	SetIgnoreMoveInput(true)
+	ShowMouseCursor(true)
+	SetInputMode(INPUT_GAMEANDUI)
+
+	lastOpened = id
+end
+
+function HideUI()
+	if dialogs[lastOpened] == nil then
+		return false
+	end
+
+	lastOpened = 0
+	ExecuteWebJS('clearUI();')
+    SetIgnoreLookInput(false)
+    SetIgnoreMoveInput(false)
+    ShowMouseCursor(false)
+    SetInputMode(INPUT_GAME)
+    SetWebVisibility(web, WEB_HITINVISIBLE)
+end
+
+function DestroyUI(id)
+    if lastOpened == id then
+        closeDialog()
+    end
+    dialogs[id] = nil
+end
+
+AddEvent("borkui:ready", function()
+    if lastOpened ~= -1 then
+        ShowUI(lastOpened)
+    end
+end)
+
+AddEvent("borkui:OnHideMenu", function()
+    if lastOpened ~= -1 then
+        Delay(1, function()
+            SetIgnoreLookInput(true)
+            SetIgnoreMoveInput(true)
+            ShowMouseCursor(true)
+            SetInputMode(INPUT_GAMEANDUI)
+        end)
+    end
+end)
+
+--[[
+	CreateUI
+	AddUITitle
+	AddUIInformation
+	AddUIDivider
+	AddUIButton
+	AddUITextInput
+	AddUIDropdown
+	ShowUI
+	HideUI
+	DestroyUI
+]]
+
+AddFunctionExport('create', CreateUI)
+AddFunctionExport('addUITitle', AddUITitle)
+AddFunctionExport('addUIInformation', AddUIInformation)
+AddFunctionExport('addUIDivider', AddUIDivider)
+AddFunctionExport('addUIButton', AddUIButton)
+AddFunctionExport('addUITextInput', AddUITextInput)
+AddFunctionExport('addUIDropdown', AddUIDropdown)
+AddFunctionExport('show', ShowUI)
+AddFunctionExport('hide', HideUI)
+AddFunctionExport('destroy', DestroyUI)
