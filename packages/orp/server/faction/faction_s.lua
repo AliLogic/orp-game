c21852cba48435935f9bd7dda34b86b70d9534cf
AliLogic <drfiniz@gmail.com>
2020-01-15 00:23:55 +0500

    add more debug lines

--- packages/orp/server/faction/faction_s.lua
+++ packages/orp/server/faction/faction_s.lua
@@ -113,8 +113,10 @@ function OnFactionLoaded(factionid)
 	if mariadb_get_row_count() == 0 then
 		print('Error with loading faction ID'..factionid)
 	else
+		print('1. Creating faction data upon being loaded')
 		CreateFactionData(factionid)
 
+		print('2. Loading the faction data from the database')
 		FactionData[factionid].id = mariadb_get_value_name_int(1, "id")
 		FactionData[factionid].name = mariadb_get_value_name(1, "name")
 		FactionData[factionid].short_name = mariadb_get_value_name(1, "short_name")
@@ -133,10 +135,14 @@ function OnFactionLoaded(factionid)
 			FactionData[factionid].locker_text3d = CreateText3D("Faction Locker (/flocker)", 10, FactionData[factionid].locker_x, FactionData[factionid].locker_y, FactionData[factionid].locker_z, 0.0, 0.0, 0.0)
 		end
 
+		print('3. Initiating the loop for faction ranks')
 		for i = 1, FactionData[factionid].leadership_rank, 1 do
 			FactionRankData[factionid][i] = {}
+			FactionRankData[factionid][i].rank_name = "Rank"..i
+			FactionRankData[factionid][i].rank_pay = 0
 		end
 
+		print('4. Selecting the faction ranks from database')
 		local query = mariadb_prepare(sql, "SELECT * FROM faction_ranks WHERE id = ? ORDER BY `rank_id` ASC", FactionData[factionid].id)
 		mariadb_async_query(sql, query, OnFactionRankLoaded, FactionData[factionid].id)
 	end
@@ -146,6 +152,7 @@ function OnFactionRankLoaded(factionid)
 	local row_count = mariadb_get_row_count()
 
 	if row_count then
+		print('5. Loading the faction ranks from database')
 		local rank_id = 0
 		for i = 1, row_count, 1 do
 			rank_id = mariadb_get_value_name_int(i, "rank_id")
