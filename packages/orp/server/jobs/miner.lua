498bcbcd4aa9dee07c77aab9b27f23ab9d68e78a
AliLogic <drfiniz@gmail.com>
2020-01-24 01:09:32 +0500

    mine shit idk

--- packages/orp/server/jobs/miner.lua
+++ packages/orp/server/jobs/miner.lua
@@ -1,3 +1,9 @@
+MineLocations = {
+	{-97382, 94231, 218},
+	{-93961, 89834, 249},
+	{-97170, 98548, 341}
+}
+
 AddCommand("mine", function (playerid)
 
 	if GetPlayerJob(playerid) ~= JOB_TYPE_MINER then
@@ -20,9 +26,17 @@ end)
 AddRemoteEvent("OnPlayerMine", function (playerid)
 
 	if (GetPlayerJob(playerid) == JOB_TYPE_MINER and PlayerData[playerid].is_mining) then
-		local money = Random(5, 25)
 
-		AddPlayerChat(playerid, "You have earned $"..money.." for the rock.");
-		AddPlayerCash(playerid, money)
+		local x, y, z = GetPlayerLocation(playerid)
+
+		for _, v in pairs(MineLocations) do
+
+			if GetDistance3D(x, y, z, MineLocations[v][1], MineLocations[v][2], MineLocations[v][3]) <= 130.0 then
+				local money = Random(5, 25)
+
+				AddPlayerChat(playerid, "You have earned $"..money.." for the rock.");
+				AddPlayerCash(playerid, money)
+			end
+		end
 	end
 end)
