1b93caff3535f5695c89cb848278a2556fe509dc
AliLogic <drfiniz@gmail.com>
2020-01-31 13:10:41 +0500

    donation updates

--- packages/orp/server/donations/donations_s.lua
+++ packages/orp/server/donations/donations_s.lua
@@ -40,6 +40,12 @@ function OnDonationsLoaded(playerid)
 
 		DonationData[playerid].level = mariadb_get_value_name_int(1, "level")
 		DonationData[playerid].date = mariadb_get_value_name_int(1, "date")
+
+		if os.time(os.date("!*t")) < DonationData[playerid].date then
+			AddPlayerChat(playerid, "Your donation level " .. DonationData[playerid].level .. " is active till " .. os.date("%m %B %Y") .. ". Thank you for helping and supporting us!")
+		else
+			AddPlayerChat(playerid, "Your donation level " .. DonationData[playerid].level .. " expired on " .. os.date("%m %B %Y") .. ". Please consider visiting the UCP for more information.")
+		end
 	end
 end
 
