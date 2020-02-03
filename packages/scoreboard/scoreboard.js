03010c96d09624525ebb5a4d56b614e678cb1959
AliLogic <drfiniz@gmail.com>
2020-02-03 21:17:31 +0500

    scoreboard code

--- packages/scoreboard/scoreboard.js
+++ packages/scoreboard/scoreboard.js
@@ -1,22 +1,73 @@
 let players = [];
-
-$(document).ready(() => {
-	//CallEvent('scorebork:ready');
-
-	insertPlayer({id:1, name:"Logic Coolguy420", level:420, ping:250});
-	insertPlayer({id:2, name:"Logic Gayguy69", level:420, ping:250});
-	insertPlayer({id:3, name:"Logic Idiote69", level:420, ping:250});
-	toggleScoreboard(true);
-});
+let selectedlast = null;
 
 const toggleScoreboard = bool => bool ? $('body').show() : $('body').hide();
 
 const removeElement = (array, elem) => {
-    var index = array.indexOf(elem);
+	var index = array.indexOf(elem);
     if (index > -1) {
         array.splice(index, 1);
 	}
 }
+
+$(document).ready(() => {
+	//CallEvent('scorebork:ready');
+
+	toggleScoreboard(true);
+
+	$('.table .body .data > .columns').on('click', function () {
+		console.log('selected');
+
+		if (selectedlast !== null) {
+			if (selectedlast.innerText === $(this).innerText && !selectedlast.hasClass('selected')) {
+				$(this).removeClass('selected');
+				console.log('selected is being deselected');
+				return;
+			}
+
+			console.log(selectedlast);
+			console.log($(this));
+
+			selectedlast.removeClass('selected');
+		}
+
+		$(this).addClass('selected');
+		selectedlast = $(this);
+
+		/*if (selectedlast !== null) {
+			if (selectedlast.css("background-color") === "rgb(255, 0, 0)") {
+				selectedlast.css("background-color", "inherit");
+
+				if (selectedlast !== $(this)) {
+					$(this).css("background-color", "red");
+				}
+				console.log('set to inherit')
+				return;
+			}
+		}
+
+		if ($(this).css("background-color") === "rgb(255, 0, 0)") {
+			$(this).css("background-color", "inherit");
+			console.log("colour inherit");
+		} else {
+			$(this).css("background-color", "red");
+			console.log("colour red");
+		}
+
+		selectedlast = $(this);*/
+
+		/*if (selectedlast !== null) {
+			if (selectedlast.css("background-color") === "rgba(0, 0, 0, 0)") {
+				selectedlast.css("background-color", "inherit");
+				selectedlast = $(this);
+				console.log('colour inherit and THIS set');
+			} else {
+				selectedlast.css("background-color", "red");
+				selectedlast = $(this);
+			}
+		}*/
+	});
+});
 
 function insertPlayer(player) {
 	players.push([player.id, players.length + 1]);
