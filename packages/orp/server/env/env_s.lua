b639da8f2144a61fcb811ba0d317b1606eab682a
AliLogic <drfiniz@gmail.com>
2020-02-11 21:38:52 +0500

    disable some code in env

--- packages/orp/server/env/env_s.lua
+++ packages/orp/server/env/env_s.lua
@@ -16,7 +16,7 @@ EnvHour = 12
 EnvMin = 0
 EnvWeather = 1
 EnvTemperature = 30
-EnvFog = 0.0
+EnvFog = 0
 
 local apikey = '4e444684a31d5a4b581548a901e6f0e8'
 local url = 'http://api.openweathermap.org/data/2.5/weather?q=Nevada,us&APPID='
@@ -67,7 +67,7 @@ function UpdateWeather()
 end
 
 AddEvent("OnPackageStart", function()
-	UpdateWeather()
+	-- UpdateWeather()
 	UpdateTime()
 	EnvTimer = CreateTimer(UpdateTime, 60 * 1000)
 end)
