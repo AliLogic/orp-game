2231825bb5187ae1511eb8d586bb30b66e384fb3
AliLogic <drfiniz@gmail.com>
2020-02-21 14:15:29 +0500

    furniture system basic code

--- packages/orp/server/furniture/cmds_s.lua
+++ packages/orp/server/furniture/cmds_s.lua
@@ -14,43 +14,11 @@ local borkui = ImportPackage("borkui")
 
 AddCommand("furniture", function (playerid)
 
-	local
-		houseid = 0
+	local houseid = Housing_Nearest(playerid)
 
-	--[[
-		Add a check to see if the player is inside the house or not
-	if houseid == 0 then
+	if houseid == 0 or (not House_IsOwner(playerid, houseid) and not PlayerHasHouseKey(playerid, houseid))then
 		return AddPlayerChat(playerid, "You are not in range of your house interior.")
 	end
-	]]--
 
-	local
-		count = 0
-	local
-		string = ""
-	local
-		x, y, z = GetPlayerLocation(playerid)
-	local
-		distance = 0
-	local
-		fx, fy, fz = 0
-
-	for i = 1, MAX_FURNITURE, 1 do
-		if (IsValidFurniture(i)) then
-			if (count < MAX_HOUSE_FURNITURE and GetFurnitureHouseID(i) == houseid) then
-
-				fx, fy, fz = GetFurnitureLocation(i)
-				distance = GetDistance3D(x, y, z, fx, fy, fz)
-
-				string = string .. GetFurnitureNameByModel(i) .. "(" .. distance .. " meters)"
-			end
-
-			if (count == 0) then
-				return AddPlayerChat(playerid, "This house doesn't have any furniture spawned in.")
-			end
-
-			borkui.createUI(playerid, 0, DIALOG_HOME_FURNITURE)
-			-- Show them the listed furniture dialog
-		end
-	end
+	borkui.createUI(playerid, 0, DIALOG_FURNITURE_MENU)
 end)
