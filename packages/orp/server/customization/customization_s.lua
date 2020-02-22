aebb70a98c73e8d0d147b3cbc7df853b191613b2
AliLogic <drfiniz@gmail.com>
2020-02-23 01:24:37 +0500

    huge clothing sys rework

--- packages/orp/server/customization/customization_s.lua
+++ packages/orp/server/customization/customization_s.lua
@@ -13,108 +13,115 @@ Contributors:
 
 PlayerClothingData = {}
 
-Body = {}
-Body[1]		= nil
-Body[2]		= "/Game/CharacterModels/Female/Meshes/SK_Female01"
-Body[3]		= "/Game/CharacterModels/Female/Meshes/SK_Female02"
+Body = {
+	"/Game/CharacterModels/Materials/HZN_Materials/M_HZN_Body_NoClothes",
+	"/Game/CharacterModels/Materials/HZN_Materials/M_HZN_Body_NoLegs",
+	"/Game/CharacterModels/Materials/HZN_Materials/M_HZN_Body_NoShoes",
+	"/Game/CharacterModels/Materials/HZN_Materials/M_HZN_Body_NoShoesLegs",
+	"/Game/CharacterModels/Materials/HZN_Materials/M_HZN_Body_NoShoesLegsTorso",
+	"/Game/CharacterModels/Materials/HZN_Materials/M_HZN_Body_NoTorso",
+	"/Game/CharacterModels/Female/Meshes/SK_Female01", -- Female
+	"/Game/CharacterModels/Female/Meshes/SK_Female02"
+}
 
-Tops = {}
-Tops[1]		= nil
-Tops[2]		= "/Game/CharacterModels/SkeletalMesh/Outfits/HZN_Outfit_Knitted_Shirt_LPR"
-Tops[3]		= "/Game/CharacterModels/SkeletalMesh/Outfits/HZN_Outfit_Piece_FormalShirt_LPR"
-Tops[4]		= "/Game/CharacterModels/SkeletalMesh/Outfits/HZN_Outfit_Piece_FormalShirt2_LPR"
-Tops[5]		= "/Game/CharacterModels/SkeletalMesh/Outfits/HZN_Outfit_Piece_Shirt_LPR"
-Tops[6]		= "/Game/CharacterModels/SkeletalMesh/Outfits/HZN_Outfit_Piece_TShirt_Knitted2_LPR"
-Tops[7]		= "/Game/CharacterModels/SkeletalMesh/Outfits/HZN_Outfit_Piece_TShirt_Knitted_LPR"
-Tops[8]		= "/Game/CharacterModels/SkeletalMesh/Outfits/HZN_Outfit_Piece_TShirt_LPR"
-Tops[9]		= "/Game/CharacterModels/SkeletalMesh/Outfits/HZN_CH3D_Prisoner_LPR"
-Tops[10]	= "/Game/CharacterModels/SkeletalMesh/Outfits/HZN_Outfit_Police_Shirt-Long_LPR"
-Tops[11]	= "/Game/CharacterModels/SkeletalMesh/Outfits/HZN_Outfit_Police_Shirt-Short_LPR"
+Tops = {
+	nil,
+	"/Game/CharacterModels/SkeletalMesh/Outfits/HZN_Outfit_Knitted_Shirt_LPR",
+	"/Game/CharacterModels/SkeletalMesh/Outfits/HZN_Outfit_Piece_FormalShirt_LPR",
+	"/Game/CharacterModels/SkeletalMesh/Outfits/HZN_Outfit_Piece_FormalShirt2_LPR",
+	"/Game/CharacterModels/SkeletalMesh/Outfits/HZN_Outfit_Piece_Shirt_LPR",
+	"/Game/CharacterModels/SkeletalMesh/Outfits/HZN_Outfit_Piece_TShirt_Knitted2_LPR",
+	"/Game/CharacterModels/SkeletalMesh/Outfits/HZN_Outfit_Piece_TShirt_Knitted_LPR",
+	"/Game/CharacterModels/SkeletalMesh/Outfits/HZN_Outfit_Piece_TShirt_LPR",
+	"/Game/CharacterModels/SkeletalMesh/Outfits/HZN_CH3D_Prisoner_LPR",
+	"/Game/CharacterModels/SkeletalMesh/Outfits/HZN_Outfit_Police_Shirt-Long_LPR",
+	"/Game/CharacterModels/SkeletalMesh/Outfits/HZN_Outfit_Police_Shirt-Short_LPR",
+	"/Game/CharacterModels/SkeletalMesh/Outfits/HZN_Outfit_Set_SpecialAgent_LPR",
+	"/Game/CharacterModels/SkeletalMesh/Outfits/HZN_Outfit_Set_Pimp_LPR",
+	"/Game/CharacterModels/SkeletalMesh/Outfits/HZN_Outfit_Set_Pimp_Open_LPR",
+	"/Game/CharacterModels/SkeletalMesh/Outfits/HZN_Outfit_Set_Police_LPR",
+	"/Game/CharacterModels/SkeletalMesh/Outfits/HZN_Outfit_Set_Scientist_LPR",
+	"/Game/CharacterModels/Mafia/Meshes/SK_Mafia",
 
-Tops[12]	= "/Game/CharacterModels/SkeletalMesh/Outfits/HZN_Outfit_Set_SpecialAgent_LPR"
-Tops[13]	= "/Game/CharacterModels/SkeletalMesh/Outfits/HZN_Outfit_Set_Pimp_LPR"
-Tops[14]	= "/Game/CharacterModels/SkeletalMesh/Outfits/HZN_Outfit_Set_Pimp_Open_LPR"
-Tops[15]	= "/Game/CharacterModels/SkeletalMesh/Outfits/HZN_Outfit_Set_Police_LPR"
-Tops[16]	= "/Game/CharacterModels/SkeletalMesh/Outfits/HZN_Outfit_Set_Scientist_LPR"
-Tops[17]	= "/Game/CharacterModels/Mafia/Meshes/SK_Mafia"
+	"/Game/CharacterModels/Clothing/Meshes/SK_Pullover",
+	"/Game/CharacterModels/Clothing/Meshes/SK_TShirt01",
+	"/Game/CharacterModels/Clothing/Meshes/SK_Undershirt01",
+	"/Game/CharacterModels/Clothing/Meshes/SK_ShirtCombo01",
+	"/Game/CharacterModels/SkeletalMesh/Outfits/HZN_Outfit_Piece_Shirt_LPR",
 
-Tops[18]	= "/Game/CharacterModels/Clothing/Meshes/SK_Pullover"
-Tops[19]	= "/Game/CharacterModels/Clothing/Meshes/SK_TShirt01"
-Tops[20]	= "/Game/CharacterModels/Clothing/Meshes/SK_Undershirt01"
-Tops[21]	= "/Game/CharacterModels/Clothing/Meshes/SK_ShirtCombo01"
+	"/Game/CharacterModels/Female/Meshes/SK_Outfit01", -- female
+	"/Game/CharacterModels/Female/Meshes/SK_Outfit02",
+	"/Game/CharacterModels/Female/Meshes/SK_Outfit03",
+	"/Game/CharacterModels/Female/Meshes/SK_Outfit04",
+	"/Game/CharacterModels/Female/Meshes/SK_Outfit05",
+	"/Game/CharacterModels/Female/Meshes/SK_Outfit06",
+	"/Game/CharacterModels/Female/Meshes/HZN_CH3D_Prison-Guard_LPR",
+	"/Game/CharacterModels/Female/Meshes/HZN_CH3D_Prisoner_LPR",
+	"/Game/CharacterModels/Female/Meshes/HZN_Outfit_Police_Shirt-Long_LPR",
+	"/Game/CharacterModels/Female/Meshes/HZN_Outfit_Police_Shirt-Short_LPR",
+}
 
-Tops[22]	= "/Game/CharacterModels/SkeletalMesh/Outfits/HZN_Outfit_Piece_Shirt_LPR"
+Equipment = {
+	"/Game/CharacterModels/Female/Meshes/HZN_Outfit_Piece_FormalJacket_LPR", -- Female
+	"/Game/CharacterModels/Female/Meshes/HZN_Outfit_Piece_FormalShirt_LPR",
+	"/Game/CharacterModels/Female/Meshes/HZN_Outfit_Piece_Shirt_LPR",
+	"/Game/CharacterModels/Female/Meshes/HZN_Outfit_Piece_Tie_LPR",
+	"/Game/CharacterModels/Female/Meshes/SK_Armor01",
+	"/Game/CharacterModels/Female/Meshes/SK_Equipment01",
+	"/Game/CharacterModels/Female/Meshes/SK_Jacket01",
+	"/Game/CharacterModels/Female/Meshes/SK_Jacket02"
+}
 
--- female
-Tops[23]	= "/Game/CharacterModels/Female/Meshes/SK_Outfit01"
-Tops[24]	= "/Game/CharacterModels/Female/Meshes/SK_Outfit02"
-Tops[25]	= "/Game/CharacterModels/Female/Meshes/SK_Outfit03"
-Tops[26]	= "/Game/CharacterModels/Female/Meshes/SK_Outfit04"
-Tops[27]	= "/Game/CharacterModels/Female/Meshes/SK_Outfit05"
-Tops[28]	= "/Game/CharacterModels/Female/Meshes/SK_Outfit06"
+Pants = {
+	nil,
+	"/Game/CharacterModels/SkeletalMesh/Outfits/HZN_Outfit_Piece_CargoPants_LPR",
+	"/Game/CharacterModels/SkeletalMesh/Outfits/HZN_Outfit_Piece_DenimPants_LPR",
+	"/Game/CharacterModels/SkeletalMesh/Outfits/HZN_Outfit_Piece_FormalPants_LPR",
+	"/Game/CharacterModels/Clothing/Meshes/SK_Jeans01",
+	"/Game/CharacterModels/Clothing/Meshes/SK_Shorts01",
+	"/Game/CharacterModels/Female/Meshes/SK_Pants01", -- Female
+	"/Game/CharacterModels/Female/Meshes/SK_Pants02",
+	"/Game/CharacterModels/Female/Meshes/HZN_Outfit_Piece_CargoPants_LPR",
+	"/Game/CharacterModels/Female/Meshes/HZN_Outfit_Piece_DenimPants_LPR",
+	"/Game/CharacterModels/Female/Meshes/HZN_Outfit_Piece_FormalPants_LPR"
+}
 
-Tops[29]	= "/Game/CharacterModels/Female/Meshes/HZN_CH3D_Prison-Guard_LPR"
-Tops[30]	= "/Game/CharacterModels/Female/Meshes/HZN_CH3D_Prisoner_LPR"
-Tops[31]	= "/Game/CharacterModels/Female/Meshes/HZN_Outfit_Piece_BusinessShoes_LPR"
-Tops[32]	= "/Game/CharacterModels/Female/Meshes/HZN_Outfit_Piece_CargoPants_LPR"
-Tops[33]	= "/Game/CharacterModels/Female/Meshes/HZN_Outfit_Piece_DenimPants_LPR"
-Tops[34]	= "/Game/CharacterModels/Female/Meshes/HZN_Outfit_Piece_FormalJacket_LPR"
-Tops[35]	= "/Game/CharacterModels/Female/Meshes/HZN_Outfit_Piece_FormalPants_LPR"
-Tops[36]	= "/Game/CharacterModels/Female/Meshes/HZN_Outfit_Piece_FormalShirt_LPR"
-Tops[37]	= "/Game/CharacterModels/Female/Meshes/HZN_Outfit_Piece_NormalShoes"
-Tops[38]	= "/Game/CharacterModels/Female/Meshes/HZN_Outfit_Piece_Shirt_LPR"
-Tops[39]	= "/Game/CharacterModels/Female/Meshes/HZN_Outfit_Piece_Tie_LPR"
-Tops[40]	= "/Game/CharacterModels/Female/Meshes/HZN_Outfit_Police_Shirt-Long_LPR"
-Tops[41]	= "/Game/CharacterModels/Female/Meshes/HZN_Outfit_Police_Shirt-Short_LPR"
-Tops[42]	= "/Game/CharacterModels/Female/Meshes/SK_Armor01"
-Tops[43]	= "/Game/CharacterModels/Female/Meshes/SK_Equipment01"
-Tops[44]	= "/Game/CharacterModels/Female/Meshes/SK_Jacket01"
-Tops[45]	= "/Game/CharacterModels/Female/Meshes/SK_Jacket02"
+Shoes = {
+	nil,
+	"/Game/CharacterModels/SkeletalMesh/Outfits/HZN_Outfit_Piece_BusinessShoes_LPR",
+	"/Game/CharacterModels/SkeletalMesh/Outfits/HZN_Outfit_Piece_NormalShoes_LPR",
+	"/Game/CharacterModels/Clothing/Meshes/SK_Shoes01",
+	"/Game/CharacterModels/Female/Meshes/SK_Shoes01", -- Female
+	"/Game/CharacterModels/Female/Meshes/SK_Shoes02",
+	"/Game/CharacterModels/Female/Meshes/HZN_Outfit_Piece_BusinessShoes_LPR",
+	"/Game/CharacterModels/Female/Meshes/HZN_Outfit_Piece_NormalShoes"
+}
 
-Pants = {}
-Pants[1]	= nil
-Pants[2] 	= "/Game/CharacterModels/SkeletalMesh/Outfits/HZN_Outfit_Piece_CargoPants_LPR"
-Pants[3]	= "/Game/CharacterModels/SkeletalMesh/Outfits/HZN_Outfit_Piece_DenimPants_LPR"
-Pants[4]	= "/Game/CharacterModels/SkeletalMesh/Outfits/HZN_Outfit_Piece_FormalPants_LPR"
-Pants[5]	= "/Game/CharacterModels/Clothing/Meshes/SK_Jeans01"
-Pants[6]	= "/Game/CharacterModels/Clothing/Meshes/SK_Shorts01"
--- female
-Pants[7]	= "/Game/CharacterModels/Female/Meshes/SK_Pants01"
-Pants[8]	= "/Game/CharacterModels/Female/Meshes/SK_Pants02"
-
-Shoes = {}
-Shoes[1]	= nil
-Shoes[2]	= "/Game/CharacterModels/SkeletalMesh/Outfits/HZN_Outfit_Piece_BusinessShoes_LPR"
-Shoes[3]	= "/Game/CharacterModels/SkeletalMesh/Outfits/HZN_Outfit_Piece_NormalShoes_LPR"
-Shoes[4]	= "/Game/CharacterModels/Clothing/Meshes/SK_Shoes01"
--- female
-Shoes[5]	= "/Game/CharacterModels/Female/Meshes/SK_Shoes01"
-Shoes[6]	= "/Game/CharacterModels/Female/Meshes/SK_Shoes02"
-
-Hair = {}
-Hair[1]		= nil
-Hair[2]		= "/Game/CharacterModels/SkeletalMesh/HZN_CH3D_Hair_Business_LP"
-Hair[3]		= "/Game/CharacterModels/SkeletalMesh/HZN_CH3D_Hair_Scientist_LP"
-Hair[4]		= "/Game/CharacterModels/SkeletalMesh/HZN_CH3D_Normal_Hair_01_LPR"
-Hair[5]		= "/Game/CharacterModels/SkeletalMesh/HZN_CH3D_Police_Hair_LPR"
-Hair[6]		= "/Game/CharacterModels/SkeletalMesh/HZN_CH3D_Normal_Hair_03_LPR"
-Hair[7]		= "/Game/CharacterModels/SkeletalMesh/HZN_CH3D_Normal_Hair_02_LPR"
--- female
-Hair[8]		= "/Game/CharacterModels/Female/Meshes/SK_Hair01"
-Hair[9]		= "/Game/CharacterModels/Female/Meshes/SK_Hair02"
-Hair[10]	= "/Game/CharacterModels/Female/Meshes/SK_Hair03"
+Hair = {
+	nil,
+	"/Game/CharacterModels/SkeletalMesh/HZN_CH3D_Hair_Business_LP",
+	"/Game/CharacterModels/SkeletalMesh/HZN_CH3D_Hair_Scientist_LP",
+	"/Game/CharacterModels/SkeletalMesh/HZN_CH3D_Normal_Hair_01_LPR",
+	"/Game/CharacterModels/SkeletalMesh/HZN_CH3D_Police_Hair_LPR",
+	"/Game/CharacterModels/SkeletalMesh/HZN_CH3D_Normal_Hair_03_LPR",
+	"/Game/CharacterModels/SkeletalMesh/HZN_CH3D_Normal_Hair_02_LPR",
+	"/Game/CharacterModels/Female/Meshes/SK_Hair01", -- female
+	"/Game/CharacterModels/Female/Meshes/SK_Hair02",
+	"/Game/CharacterModels/Female/Meshes/SK_Hair03"
+}
 
 -- Functions
 
 function CreatePlayerClothingData(player)
 	PlayerClothingData[player] = {}
 
-	PlayerClothingData[player].hair = 0
+	PlayerClothingData[player].hair = 1
 	PlayerClothingData[player].hair_color = RGB(0, 0, 0)
-	PlayerClothingData[player].top = 0
-	PlayerClothingData[player].pants = 0
-	PlayerClothingData[player].shoes = 0
+	PlayerClothingData[player].top = 1
+	PlayerClothingData[player].pants = 1
+	PlayerClothingData[player].shoes = 1
 	PlayerClothingData[player].skin_color = RGB(255, 255, 255)
+	PlayerClothingData[player].body = 1
 end
 
 function DestroyPlayerClothingData(player)
@@ -163,13 +170,14 @@ function SavePlayerClothing(player)
 		return
 	end
 
-	local query = mariadb_prepare(sql, "UPDATE clothing SET hair_color = '?', hair = ?, top = ?, pants = ?, shoes = ?, skin_color = '?' WHERE id = ? LIMIT 1;",
+	local query = mariadb_prepare(sql, "UPDATE clothing SET hair_color = '?', hair = ?, top = ?, pants = ?, shoes = ?, skin_color = '?', body = '?' WHERE id = ? LIMIT 1;",
 		PlayerClothingData[player].hair_color,
 		PlayerClothingData[player].hair,
 		PlayerClothingData[player].top,
 		PlayerClothingData[player].pants,
 		PlayerClothingData[player].shoes,
 		PlayerClothingData[player].skin_color,
+		PlayerClothingData[player].body,
 		PlayerData[player].id
 	)
 	mariadb_query(sql, query)
@@ -186,6 +194,7 @@ local function OnClothingLoad(player)
 		PlayerClothingData[player].pants = mariadb_get_value_name_int(1, "pants")
 		PlayerClothingData[player].shoes = mariadb_get_value_name_int(1, "shoes")
 		PlayerClothingData[player].skin_color = mariadb_get_value_name_int(1, "skin_color")
+		PlayerClothingData[player].body = mariadb_get_value_name_int(1, "body")
 	else
 		CreatePlayerClothing(player)
 	end
@@ -219,7 +228,7 @@ end)
 AddCommand("hair", function (playerid, hairid)
 
 	if hairid == nil then
-		return AddPlayerChat(playerid, "/hair <1 - 10>")
+		return AddPlayerChat(playerid, "/hair <1 - "..#Hair..">")
 	end
 
 	hairid = tonumber(hairid)
@@ -230,7 +239,7 @@ end)
 AddCommand("pants", function (playerid, pantid)
 
 	if pantid == nil then
-		return AddPlayerChat(playerid, "/pants <1 - 8>")
+		return AddPlayerChat(playerid, "/pants <1 - "..#Pants..">")
 	end
 
 	pantid = tonumber(pantid)
@@ -241,7 +250,7 @@ end)
 AddCommand("shirt", function (playerid, shirtid)
 
 	if shirtid == nil then
-		return AddPlayerChat(playerid, "/shirt <1 - 45>")
+		return AddPlayerChat(playerid, "/shirt <1 - "..#Tops..">")
 	end
 
 	shirtid = tonumber(shirtid)
@@ -252,7 +261,7 @@ end)
 AddCommand("shoe", function (playerid, shoeid)
 
 	if shoeid == nil then
-		return AddPlayerChat(playerid, "/shoe <1 - 6>")
+		return AddPlayerChat(playerid, "/shoe <1 - "..#Shoes..">")
 	end
 	shoeid = tonumber(shoeid)
 
@@ -288,11 +297,11 @@ AddCommand("pupil", function (playerid, pupil)
 	SetPlayerClothing(playerid, playerid)
 end)
 
-AddCommand("female", function (player, body)
+AddCommand("body", function (player, body)
 	--CallRemoteEvent(player, 'SetPlayerFemale', player)
 
 	if body == nil then
-		return AddPlayerChat(player, "/female <1 - 3>")
+		return AddPlayerChat(player, "/body <1 - 8>")
 	end
 
 	body = tonumber(body)
