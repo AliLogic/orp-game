414f268f9a649d2b18b5787afcafa917750f2160
AliLogic <drfiniz@gmail.com>
2020-02-23 13:39:06 +0500

    customization_c cleanup

--- packages/orp/client/customization/customization_c.lua
+++ packages/orp/client/customization/customization_c.lua
@@ -13,14 +13,10 @@ Contributors:
 
 local function SetPlayerClothing(player, part, piece, r, g, b, a)
 
-	local SkeletalMeshComponent
-	local pieceName
+	local SkeletalMeshComponent = GetPlayerSkeletalMeshComponent(player, "Clothing"..part)
 
-	SkeletalMeshComponent = GetPlayerSkeletalMeshComponent(player, "Clothing"..part)
-	pieceName = piece
-
-	SkeletalMeshComponent:SetSkeletalMesh(USkeletalMesh.LoadFromAsset(pieceName))
-	local DynamicMaterialInstance = SkeletalMeshComponent:CreateDynamicMaterialInstance(0)
+	SkeletalMeshComponent:SetSkeletalMesh(USkeletalMesh.LoadFromAsset(piece))
+	-- local DynamicMaterialInstance = SkeletalMeshComponent:CreateDynamicMaterialInstance(0)
 
 	if part == 0 then
 		SkeletalMeshComponent:SetColorParameterOnMaterials("Hair Color", FLinearColor(r / 255, g / 255, b / 255, a / 255))
