c74099822375931029bcfb183e7cd894b81bc707
AliLogic <drfiniz@gmail.com>
2020-02-24 00:56:39 +0500

    ATM server side code

--- packages/orp/server/banking/banking_s.lua
+++ packages/orp/server/banking/banking_s.lua
@@ -1,11 +1,26 @@
+--[[
+Copyright (C) 2019 Onset Roleplay
+
+Developers:
+* Bork
+* Logic_
+
+Contributors:
+* Blue Mountains GmbH
+]]--
+
+-- Variables
+
 local colour = ImportPackage('colours')
 
-AtmObjectsCached = { }
+-- Variables
+
+MAX_ATM = 50
 ATMData = { }
 
 local CONFIG_ATM = {
     withdraw = {
-        min = 1, 
+        min = 1,
         max = 2500
     },
     deposit = {
@@ -14,30 +29,95 @@ local CONFIG_ATM = {
     }
 }
 
+-- Functions
+
+local function CreateAtmData(index)
+
+	ATMData[index] = {}
+
+	ATMData[index].x = 0
+	ATMData[index].y = 0
+	ATMData[index].z = 0
+end
+
+local function GetFreeAtmId()
+
+	for i = 1, MAX_ATM, 1 do
+		if ATMData[i] == nil then
+			CreateAtmData(i)
+			return i
+		end
+	end
+
+	return 0
+end
+
+local function OnAtmCreated(index, x, y, z, h)
+
+	ATMData[index].id = mariadb_get_insert_id()
+
+	ATMData[index].x = x
+	ATMData[index].y = y
+	ATMData[index].z = z
+
+	ATMData[index].text3d = CreateText3D("ATM ("..ATMData[index].id..")\n/atm", 18, ATMData[index].x, ATMData[index].y, ATMData[index].z + 200, 0, 0, 0)
+end
+
+function ATM_Create(player, x, y, z, h)
+	local index = GetFreeHousingId()
+	if index == 0 then
+		return false
+	end
+
+	local query = mariadb_prepare(sql, "INSERT INTO atm VALUES(?, ?, ?, ?, 0.0, ?, 0.0);", 494, x, y, z, h)
+	mariadb_async_query(sql, query, OnAtmCreated, index, x, y, z, h)
+end
+
+function ATM_Nearest(playerid)
+	local x, y, z = GetPlayerLocation(playerid)
+	local distance = 0
+
+	for v = 1, #ATMData, 1 do
+		if ATMData[v] ~= nil then
+			distance = GetDistance3D(x, y, z, ATMData[v].x, ATMData[v].y, ATMData[v].z)
+
+			if distance <= 200.0 then
+				return v
+			end
+		end
+	end
+
+	return 0
+end
+
+local function LoadATM(i)
+
+	local index = GetFreeAtmId()
+
+	if index == 0 then
+		return false
+	end
+
+	local result = mariadb_get_assoc(i)
+
+	ATMData[index].id = math.tointeger(result["id"])
+	--local modelid = math.tointeger(result["modelid"])
+	ATMData[index].x = tonumber(result["x"])
+	ATMData[index].y = tonumber(result["y"])
+	ATMData[index].z = tonumber(result["z"])
+	--local rx = tonumber(result["rx"])
+	--local ry = tonumber(result["ry"])
+	--local rz = tonumber(result["rz"])
+
+	ATMData[index].text3d = CreateText3D("ATM ("..ATMData[index].id..")\n/atm", 18, ATMData[index].x, ATMData[index].y, ATMData[index].z + 200, 0, 0, 0)
+end
+
 function OnAtmLoaded()
 	for i = 1, mariadb_get_row_count() do
-		local result = mariadb_get_assoc(i)
-
-		local id = math.tointeger(result["id"])
-		local modelid = math.tointeger(result["modelid"])
-		local x = tonumber(result["x"])
-		local y = tonumber(result["y"])
-		local z = tonumber(result["z"])
-		local rx = tonumber(result["rx"])
-		local ry = tonumber(result["ry"])
-		local rz = tonumber(result["rz"])
-
-		CreateATM(id, modelid, x, y, z, rx, ry, rz)
+		LoadATM(i)
 	end
 
 	print("** ATMs Loaded: " .. #ATMData .. ".")
-end
-
-function CreateATM(id, modelid, x, y, z, rx, ry, rz)
-	ATMData[id] = {}
-	ATMData[id].text3d = CreateText3D("ATM\n/atm", 18, x, y, z + 200, 0, 0, 0)
-
-	table.insert(AtmObjectsCached, ATMData[id].object)
 end
 
 function GetAtmByObject(atmobject)
@@ -55,20 +135,6 @@ AddEvent("LoadATMs", function()
 	mariadb_async_query(sql, "SELECT * FROM atm;", OnAtmLoaded)
 end)
 
-AddRemoteEvent("banking:atminteract", function(player, atmobject)
-	local atm = GetAtmByObject(atmobject)
-	if atm then
-		local x, y, z = GetObjectLocation(atm.object)
-		local x2, y2, z2 = GetPlayerLocation(player)
-		local dist = GetDistance3D(x, y, z, x2, y2, z2)
-
-        if dist < 200 then
-            CallRemoteEvent(player, "iwb:opengui", GetPlayerCash(player), GetPlayerBankCash(player))
-			--webgui.ShowInputBox(player, "Balance: "..FormatMoney(PlayerData[player].bank_balance).."<br><br>Withdraw money", "Withdraw", "OnBankingWithdrawMoney")
-		end
-	end
-end)
-
 AddRemoteEvent("iwb:OnClientDeposit", function (playerid, amount)
 
 	if amount > GetPlayerCash(playerid) then
@@ -81,7 +147,7 @@ AddRemoteEvent("iwb:OnClientDeposit", function (playerid, amount)
     AddPlayerChat(playerid, "<span color=\""..colour.COLOUR_DARKGREEN().."\">Amount Deposited: $"..amount.."</>")
 	AddPlayerChat(playerid, "<span color=\""..colour.COLOUR_DARKGREEN().."\">New Balance: $".. GetPlayerBankCash(playerid) .."</>")
 	AddPlayerChat(playerid, "<span color=\""..colour.COLOUR_DARKGREEN().."\">ATM: Thank you for using our services at the Imminent Wealth Bank, see you soon!</>")
-	
+
 	CallRemoteEvent(playerid, "iwb:OnServerATMAction", GetPlayerBankCash(playerid))
 end)
 
@@ -97,11 +163,16 @@ AddRemoteEvent("iwb:OnClientWithdraw", function (playerid, amount)
     AddPlayerChat(playerid, "<span color=\""..colour.COLOUR_DARKGREEN().."\">Amount Withdrawn: $"..amount.."</>")
     AddPlayerChat(playerid, "<span color=\""..colour.COLOUR_DARKGREEN().."\">New Balance: $".. GetPlayerBankCash(playerid) .."</>")
 	AddPlayerChat(playerid, "<span color=\""..colour.COLOUR_DARKGREEN().."\">ATM: Thank you for using our services at the Imminent Wealth Bank, see you soon!</>")
-	
+
 	CallRemoteEvent(playerid, "iwb:OnServerATMAction", GetPlayerBankCash(playerid))
 end)
 
 -- Commands
 AddCommand("atm", function (player)
-    CallRemoteEvent(player, "iwb:opengui", GetPlayerCash(player), GetPlayerBankCash(player))
+
+	if ATM_Nearest(player) ~= 0 then
+		return AddPlayerChatError(player, "You are not near any ATMs.")
+	end
+
+	CallRemoteEvent(player, "iwb:opengui", GetPlayerCash(player), GetPlayerBankCash(player))
 end)
--- packages/orp/server/banking/cmds_s.lua
+++ packages/orp/server/banking/cmds_s.lua
@@ -3,32 +3,16 @@ local colour = ImportPackage('colours')
 AddCommand("acreateatm", function (player)
     if (PlayerData[player].admin < 5) then
         return AddPlayerChatError(player, "You don't have permission to use this command.")
-    end
+	end
 
 	local x, y, z = GetPlayerLocation(player)
 	local h = GetPlayerHeading(player)
-	local modelid = 494
 
-	local query = mariadb_prepare(sql, "INSERT INTO atm VALUES(?, ?, ?, ?, 0.0, ?, 0.0);",
-		modelid,
-		x, y, z, h)
+	local atm = ATM_Create(player, x, y, z, h)
 
-	mariadb_query(sql, query, OnAtmAdded, player, modelid, x, y, z, h)
-end)
-
-function OnAtmAdded(player, modelid, x, y, z, h)
-	local id = mariadb_get_insert_id()
-
-	if id ~= false then
-		AddPlayerChat(player, "<span color=\""..colour.COLOUR_LIGHTRED().."\">Server:</> ATM ID "..id.." created successfully!")
-
-		CreateATM(id, modelid, x, y, z, 0.0, h, 0.0)
-
-		-- Tell clients
-		for _, v in pairs(GetAllPlayers()) do
-			CallRemoteEvent(v, "banking:atmsetup", AtmObjectsCached)
-		end
-	else
-		AddPlayerChat(player, "<span color=\""..colour.COLOUR_LIGHTRED().."\">Error:</> Your ATM couldn't be created.")
+	if atm == false then
+		return AddPlayerChatError(player, "Maximum ATMs ("..MAX_ATM..") on the server are created.")
 	end
-end
+
+	AddPlayerChat(player, string.format("<span color=\"%s\">Server: </>ATM (ID: %d) created successfully!", colour.COLOUR_LIGHTRED(), atm))
+end)
