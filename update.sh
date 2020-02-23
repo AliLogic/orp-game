0894db294f5ec5a35da721fa174c4569ffd64600
bork <alexrahali3@gmail.com>
2020-02-23 16:36:34 +0000

    last update and fix

--- update.sh
+++ update.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-git pull http://onsetroleplay:GSbU0oQa8ReVBE8ZKDj65bUz93!@68.183.37.59:4092/orp/game.git
+git pull http://onsetroleplay:GSbU0oQa8ReVBE8ZKDj65bUz93.@68.183.37.59:4092/orp/game.git
