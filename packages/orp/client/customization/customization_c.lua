e4d12584f01a7059772a71ccd4c4aa1a8817aa75
AliLogic <drfiniz@gmail.com>
2020-02-28 18:25:46 +0500

    lua workaround

--- packages/orp/client/customization/customization_c.lua
+++ packages/orp/client/customization/customization_c.lua
@@ -76,7 +76,7 @@ AddRemoteEvent("LoadPlayerClothingTextures", function (textures)
 
 	AddPlayerChat("textures init size: "..#TexturesLoaded)
 
-	table.insert(TexturesLoaded, nil)
+	table.insert(TexturesLoaded, 0) -- this is a workaround as Lua uses nil to empty/ reset tables
 	for k, v in pairs(textures) do
 		table.insert(TexturesLoaded, UTexture2D.LoadFromFile(v))
 	end
