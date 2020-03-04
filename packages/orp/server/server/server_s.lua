9818355d5ab5bcc45345af05bd5e0e0bd7fe3ed2
AliLogic <drfiniz@gmail.com>
2020-03-04 14:33:51 +0500

    fix server data query

--- packages/orp/server/server/server_s.lua
+++ packages/orp/server/server/server_s.lua
@@ -13,9 +13,9 @@ local function OnLoadServerData()
 	}
 
 	if mariadb_get_row_count() ~= 0 then
-		ServerData.tax = mariadb_get_value_name_float(1, "tax")
-		ServerData.tax_percent = mariadb_get_value_name_float(1, "tax_percent")
-		ServerData.double_xp = mariadb_get_value_name_float(1, "double_xp")
+		ServerData.tax = mariadb_get_value_name_int(1, "tax")
+		ServerData.tax_percent = mariadb_get_value_name_int(1, "tax_percent")
+		ServerData.double_xp = mariadb_get_value_name_int(1, "double_xp")
 	end
 
 	print("Tax: " .. ServerData.tax .. "")
@@ -60,6 +60,6 @@ end)
 
 AddEvent('UnloadServer', function ()
 
-	local query = mariadb_prepare(sql, "UPDATE server SET tax = ?, tax_percent = ? WHERE server = 'server'")
+	local query = mariadb_prepare(sql, "UPDATE server SET tax = ?, tax_percent = ? WHERE server = 'server'", ServerData.tax, ServerData.tax_percent)
 	mariadb_query(sql, query)
 end)
